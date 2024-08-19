879c32afa89adf4ab30b4f8a262e80e5e732b9c8
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-08-19 18:21:03 +0200

    Assert on expected exception's message (#160)

--- polaris-service/src/test/java/io/polaris/service/PolarisApplicationIntegrationTest.java
+++ polaris-service/src/test/java/io/polaris/service/PolarisApplicationIntegrationTest.java
@@ -308,7 +308,9 @@ public class PolarisApplicationIntegrationTest {
   @Test
   public void testConfigureCatalogCaseSensitive() throws IOException {
     assertThatThrownBy(() -> newSessionCatalog("TESTCONFIGURECATALOGCASESENSITIVE"))
-        .isInstanceOf(RESTException.class);
+        .isInstanceOf(RESTException.class)
+        .hasMessage(
+            "Unable to process: Unable to find warehouse TESTCONFIGURECATALOGCASESENSITIVE");
   }
 
   @Test
@@ -318,7 +320,8 @@ public class PolarisApplicationIntegrationTest {
       SessionCatalog.SessionContext sessionContext = SessionCatalog.SessionContext.createEmpty();
       assertThatThrownBy(
               () -> sessionCatalog.listNamespaces(sessionContext, Namespace.of("whoops")))
-          .isInstanceOf(NoSuchNamespaceException.class);
+          .isInstanceOf(NoSuchNamespaceException.class)
+          .hasMessage("Namespace does not exist: whoops");
     }
   }
 
@@ -334,7 +337,8 @@ public class PolarisApplicationIntegrationTest {
               () ->
                   sessionCatalog.listNamespaces(
                       sessionContext, Namespace.of("top_level", "whoops")))
-          .isInstanceOf(NoSuchNamespaceException.class);
+          .isInstanceOf(NoSuchNamespaceException.class)
+          .hasMessage("Namespace does not exist: top_level.whoops");
     }
   }
 
@@ -344,7 +348,8 @@ public class PolarisApplicationIntegrationTest {
         newSessionCatalog("testIcebergListTablesNamespaceNotFound")) {
       SessionCatalog.SessionContext sessionContext = SessionCatalog.SessionContext.createEmpty();
       assertThatThrownBy(() -> sessionCatalog.listTables(sessionContext, Namespace.of("whoops")))
-          .isInstanceOf(NoSuchNamespaceException.class);
+          .isInstanceOf(NoSuchNamespaceException.class)
+          .hasMessage("Namespace does not exist: whoops");
     }
   }
 
@@ -394,7 +399,8 @@ public class PolarisApplicationIntegrationTest {
       assertThat(namespaces).isNotNull().hasSize(1).containsExactly(ns);
       sessionCatalog.dropNamespace(sessionContext, ns);
       assertThatThrownBy(() -> sessionCatalog.loadNamespaceMetadata(sessionContext, ns))
-          .isInstanceOf(NoSuchNamespaceException.class);
+          .isInstanceOf(NoSuchNamespaceException.class)
+          .hasMessage("Namespace does not exist: db1");
     }
   }
 
@@ -420,7 +426,8 @@ public class PolarisApplicationIntegrationTest {
                       .withSortOrder(SortOrder.unsorted())
                       .withPartitionSpec(PartitionSpec.unpartitioned())
                       .create())
-          .isInstanceOf(BadRequestException.class);
+          .isInstanceOf(BadRequestException.class)
+          .hasMessage("Malformed request: Cannot create table on external catalogs.");
     }
   }
 
@@ -570,7 +577,8 @@ public class PolarisApplicationIntegrationTest {
                               new PartitionData(PartitionSpec.unpartitioned().partitionType()),
                               10L))
                       .commit())
-          .isInstanceOf(BadRequestException.class);
+          .isInstanceOf(BadRequestException.class)
+          .hasMessage("Malformed request: Cannot update table on external catalogs.");
     }
   }
 
@@ -614,7 +622,8 @@ public class PolarisApplicationIntegrationTest {
       assertThat(table).isNotNull();
       sessionCatalog.dropTable(sessionContext, tableIdentifier);
       assertThatThrownBy(() -> sessionCatalog.loadTable(sessionContext, tableIdentifier))
-          .isInstanceOf(NoSuchTableException.class);
+          .isInstanceOf(NoSuchTableException.class)
+          .hasMessage("Table does not exist: db1.the_table");
     }
   }
 
@@ -638,7 +647,8 @@ public class PolarisApplicationIntegrationTest {
                           emptyEnvironmentVariable,
                           "header." + REALM_PROPERTY_KEY,
                           realm)))
-          .isInstanceOf(BadRequestException.class);
+          .isInstanceOf(BadRequestException.class)
+          .hasMessage("Malformed request: Please specify a warehouse");
     }
   }
 }
