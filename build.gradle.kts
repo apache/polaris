3cf0880644f38608cb98b5c8db73632804476a85
Eric Maynard <eric.maynard+oss@snowflake.com>
2024-09-03 11:27:03 -0700

    revert test changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,8 +56,6 @@ if (System.getProperty("idea.sync.active").toBoolean()) {
 eclipse { project { name = ideName } }
 
 tasks.named<RatTask>("rat").configure {
-  onlyIf { project.findProperty("skipRat")?.toString()?.toBoolean() != true }
-
   // These are Gradle file pattern syntax
   excludes.add("**/build/**")
 
