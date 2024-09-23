ca7c5850476da9bf6e7193539634344c559dd534
Yong <yong@Yongs-Air.home>
2024-09-22 23:24:10 -0500

    Add eclipse link deps for Dockerfile and run.sh

--- run.sh
+++ run.sh
@@ -26,8 +26,8 @@ BUILD_ARGS=""  # Initialize an empty string to store Docker build arguments
 
 # Function to display usage information
 usage() {
-  echo "Usage: $0 [-b build-arg1=value1,build-arg2=value2,...] [-h]"
-  echo "  -b    Pass a set of arbitrary build arguments to docker build, separated by commas"
+  echo "Usage: $0 [-b build-arg1=value1;build-arg2=value2;...] [-h]"
+  echo "  -b    Pass a set of arbitrary build arguments to docker build, separated by semicolons"
   echo "  -h    Display this help message"
   exit 1
 }
@@ -36,7 +36,7 @@ usage() {
 while getopts "b:h" opt; do
   case ${opt} in
     b)
-      IFS=',' read -ra ARGS <<< "${OPTARG}"  # Split the comma-separated list into an array
+      IFS=';' read -ra ARGS <<< "${OPTARG}"  # Split the semicolon-separated list into an array
       for arg in "${ARGS[@]}"; do
         BUILD_ARGS+=" --build-arg ${arg}"  # Append each build argument to the list
       done
@@ -55,7 +55,7 @@ shift $((OPTIND-1))
 
 # Deploy the registry
 echo "Building Kind Registry..."
-sh ./kind-registry.sh
+#sh ./kind-registry.sh
 
 # Check if BUILD_ARGS is not empty and print the build arguments
 if [[ -n "$BUILD_ARGS" ]]; then
@@ -68,10 +68,10 @@ fi
 echo "Building polaris image..."
 docker build -t localhost:5001/polaris $BUILD_ARGS -f Dockerfile .
 echo "Pushing polaris image..."
-docker push localhost:5001/polaris
+#docker push localhost:5001/polaris
 echo "Loading polaris image to kind..."
-kind load docker-image localhost:5001/polaris:latest
+#kind load docker-image localhost:5001/polaris:latest
 
 echo "Applying kubernetes manifests..."
-kubectl delete -f k8/deployment.yaml --ignore-not-found
-kubectl apply -f k8/deployment.yaml
+#kubectl delete -f k8/deployment.yaml --ignore-not-found
+#kubectl apply -f k8/deployment.yaml
