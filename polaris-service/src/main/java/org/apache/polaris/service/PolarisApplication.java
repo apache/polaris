4e25cb2c0477434b089b063b1715dfb11535e5a7
Eric Maynard <eric.maynard+oss@snowflake.com>
2024-10-03 11:20:06 -0700

    register mapper

--- polaris-service/src/main/java/org/apache/polaris/service/PolarisApplication.java
+++ polaris-service/src/main/java/org/apache/polaris/service/PolarisApplication.java
@@ -103,6 +103,7 @@ import org.apache.polaris.service.context.RealmContextResolver;
 import org.apache.polaris.service.exception.IcebergExceptionMapper;
 import org.apache.polaris.service.exception.IcebergJerseyViolationExceptionMapper;
 import org.apache.polaris.service.exception.IcebergJsonProcessingExceptionMapper;
+import org.apache.polaris.service.exception.PolarisExceptionMapper;
 import org.apache.polaris.service.persistence.InMemoryPolarisMetaStoreManagerFactory;
 import org.apache.polaris.service.ratelimiter.RateLimiterFilter;
 import org.apache.polaris.service.storage.PolarisStorageIntegrationProviderImpl;
@@ -280,6 +281,7 @@ public class PolarisApplication extends Application<PolarisApplicationConfig> {
     }
     environment.jersey().register(new IcebergRestOAuth2Api(oauth2Service));
     environment.jersey().register(new IcebergExceptionMapper());
+    environment.jersey().register(new PolarisExceptionMapper());
     PolarisServiceImpl polarisService = new PolarisServiceImpl(entityManagerFactory, authorizer);
     environment.jersey().register(new PolarisCatalogsApi(polarisService));
     environment.jersey().register(new PolarisPrincipalsApi(polarisService));
