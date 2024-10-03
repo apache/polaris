fe191591c2487cd2a29f9fcf45f43bf5190c44a8
Eric Maynard <eric.maynard+oss@snowflake.com>
2024-10-03 11:03:11 -0700

    revert

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -63,9 +63,6 @@ pluginManagement {
     gradlePluginPortal()
   }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
 
 dependencyResolutionManagement {
   repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
