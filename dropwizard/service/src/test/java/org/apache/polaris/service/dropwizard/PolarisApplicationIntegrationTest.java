4773db4e5568f1f10c6e596904019a935d439c2f
sullis <github@seansullivan.com>
2024-10-18 11:47:26 -0700

    remove Exception from assertion

--- dropwizard/service/src/test/java/org/apache/polaris/service/dropwizard/PolarisApplicationIntegrationTest.java
+++ dropwizard/service/src/test/java/org/apache/polaris/service/dropwizard/PolarisApplicationIntegrationTest.java
@@ -210,7 +210,7 @@ public class PolarisApplicationIntegrationTest {
         .exists()
         .content()
         .hasSizeGreaterThan(0)
-        .doesNotContain("ERROR", "FATAL", "Exception");
+        .doesNotContain("ERROR", "FATAL");
   }
 
   /**
