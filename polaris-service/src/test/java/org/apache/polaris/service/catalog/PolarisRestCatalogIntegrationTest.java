6b50de8583527bed8baa003e3a8b1839137bf180
Dennis Huo <7410123+dennishuo@users.noreply.github.com>
2024-10-31 13:02:59 -0700

    Improve the error message in the exception thrown for credential-vending enablement (#414)
    * Improve the error message in the exception thrown for credential-vending enablement
    
    Instead of "is not supported", phrase it as "is not enabled", and provide the catalog config
    key the user can look up to decide whether to enable the feature for their catalog.
    
    * Update test to take the constant from PolarisConfiguration

--- polaris-service/src/test/java/org/apache/polaris/service/catalog/PolarisRestCatalogIntegrationTest.java
+++ polaris-service/src/test/java/org/apache/polaris/service/catalog/PolarisRestCatalogIntegrationTest.java
@@ -713,7 +713,9 @@ public class PolarisRestCatalogIntegrationTest extends CatalogTests<RESTCatalog>
         Assertions.assertThatThrownBy(
                 () -> restCatalog.loadTable(TableIdentifier.of(ns1, "my_table")))
             .isInstanceOf(ForbiddenException.class)
-            .hasMessageContaining("Access Delegation is not supported for this catalog");
+            .hasMessageContaining("Access Delegation is not enabled for this catalog")
+            .hasMessageContaining(
+                PolarisConfiguration.ALLOW_EXTERNAL_CATALOG_CREDENTIAL_VENDING.catalogConfig());
       } finally {
         resolvingFileIO.deleteFile(fileLocation);
       }
