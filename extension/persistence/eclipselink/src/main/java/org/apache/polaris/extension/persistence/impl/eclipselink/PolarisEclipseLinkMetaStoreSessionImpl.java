b1e60062a98d16b967bd6da3e0df1935df2e0b84
rahul <rahul.prakash@fivetran.com>
2024-11-21 08:08:13 +0530

    Fetching jdbc url from the env

--- extension/persistence/eclipselink/src/main/java/org/apache/polaris/extension/persistence/impl/eclipselink/PolarisEclipseLinkMetaStoreSessionImpl.java
+++ extension/persistence/eclipselink/src/main/java/org/apache/polaris/extension/persistence/impl/eclipselink/PolarisEclipseLinkMetaStoreSessionImpl.java
@@ -19,9 +19,9 @@
 package org.apache.polaris.extension.persistence.impl.eclipselink;
 
 import static org.eclipse.persistence.config.PersistenceUnitProperties.ECLIPSELINK_PERSISTENCE_XML;
+import static org.eclipse.persistence.config.PersistenceUnitProperties.JDBC_PASSWORD;
 import static org.eclipse.persistence.config.PersistenceUnitProperties.JDBC_URL;
 import static org.eclipse.persistence.config.PersistenceUnitProperties.JDBC_USER;
-import static org.eclipse.persistence.config.PersistenceUnitProperties.JDBC_PASSWORD;
 
 import com.google.common.base.Predicates;
 import jakarta.persistence.EntityManager;
@@ -178,9 +178,10 @@ public class PolarisEclipseLinkMetaStoreSessionImpl implements PolarisMetaStoreS
       if (properties.containsKey(JDBC_URL)) {
         properties.put(JDBC_URL, properties.get(JDBC_URL).replace("{realm}", realm));
       }
-      //Fivetran: Changes to retrieve username and password from environment variables
+      // Fivetran: Changes to retrieve username and password from environment variables
       properties.put(JDBC_USER, System.getenv("POLARIS_DB_USER"));
       properties.put(JDBC_PASSWORD, System.getenv("POLARIS_DB_PASSWORD"));
+      properties.put(JDBC_URL, System.getenv("POLARIS_JDBC_URL"));
       properties.put(ECLIPSELINK_PERSISTENCE_XML, confFile);
 
       factory = Persistence.createEntityManagerFactory(persistenceUnitName, properties);
