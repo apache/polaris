89d2e2b4dfe6e9e5d6404f091142176747faa214
Michael Collado <40346148+collado-mike@users.noreply.github.com>
2024-12-02 13:53:22 -0800

    Update polaris-core/src/main/java/org/apache/polaris/core/context/RealmScope.java
    Co-authored-by: Robert Stupp <snazy@snazy.de>

--- polaris-core/src/main/java/org/apache/polaris/core/context/RealmScope.java
+++ polaris-core/src/main/java/org/apache/polaris/core/context/RealmScope.java
@@ -24,6 +24,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Scope
+@Documented
 @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface RealmScope {}
