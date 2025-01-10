7d3fc432411189e1090908d5b0a7b904b2de73a5
Mend Renovate <renovate@whitesourcesoftware.com>
2025-01-10 10:15:39 +0100

    main: Update dependency io.netty:netty-codec-http2 to v4.1.116.Final (#646)

--- polaris-core/build.gradle.kts
+++ polaris-core/build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
   implementation("com.azure:azure-identity")
   implementation("com.azure:azure-storage-file-datalake")
   constraints {
-    implementation("io.netty:netty-codec-http2:4.1.100") {
+    implementation("io.netty:netty-codec-http2:4.1.116.Final") {
       because("Vulnerability detected in 4.1.72")
     }
     implementation("io.projectreactor.netty:reactor-netty-http:1.1.13") {
