53532b96755596ac96605930d7813f3e57da3372
Robert Stupp <snazy@snazy.de>
2025-01-11 16:41:13 +0100

    Publish test-fixtures sources + javadoc as well
    Publish the sources and javadoc for test-fixtures in addition to the already published jar.

--- build-logic/src/main/kotlin/publishing/PublishingHelperPlugin.kt
+++ build-logic/src/main/kotlin/publishing/PublishingHelperPlugin.kt
@@ -27,6 +27,8 @@ import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
 import org.gradle.api.publish.tasks.GenerateModuleMetadata
+import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.api.tasks.javadoc.Javadoc
 import org.gradle.jvm.tasks.Jar
 import org.gradle.kotlin.dsl.*
 import org.gradle.plugins.signing.SigningExtension
@@ -132,9 +134,27 @@ constructor(private val softwareComponentFactory: SoftwareComponentFactory) : Pl
 
                 suppressPomMetadataWarningsFor("testFixturesApiElements")
                 suppressPomMetadataWarningsFor("testFixturesRuntimeElements")
+              }
 
-                mavenPublication.groupId = "$group"
-                mavenPublication.version = project.version.toString()
+              if (
+                plugins.hasPlugin("java-test-fixtures") &&
+                  project.layout.projectDirectory.dir("src/testFixtures").asFile.exists()
+              ) {
+                val testFixturesSourcesJar by
+                  tasks.registering(org.gradle.api.tasks.bundling.Jar::class) {
+                    val sourceSets: SourceSetContainer by project
+                    from(sourceSets.named("testFixtures").get().allSource)
+                    archiveClassifier.set("test-fixtures-sources")
+                  }
+                tasks.named<Javadoc>("testFixturesJavadoc") { isFailOnError = false }
+                val testFixturesJavadocJar by
+                  tasks.registering(org.gradle.api.tasks.bundling.Jar::class) {
+                    from(tasks.named("testFixturesJavadoc"))
+                    archiveClassifier.set("test-fixtures-javadoc")
+                  }
+
+                artifact(testFixturesSourcesJar)
+                artifact(testFixturesJavadocJar)
               }
 
               tasks.named("generatePomFileForMavenPublication").configure {
