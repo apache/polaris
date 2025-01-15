1c0aab5a0592b6e17723d3db4d6c39e422619209
Alexandre Dutra <adutra@users.noreply.github.com>
2025-01-15 15:40:09 +0100

    Reinstate legacy management endpoints
    This PR puts back the legacy endpoints /healthcheck and /metrics on the management port.

--- quarkus/service/src/test/java/org/apache/polaris/service/quarkus/TimedApplicationEventListenerTest.java
+++ quarkus/service/src/test/java/org/apache/polaris/service/quarkus/TimedApplicationEventListenerTest.java
@@ -40,10 +40,11 @@ import org.hawkular.agent.prometheus.types.Summary;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @QuarkusTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -80,11 +81,12 @@ public class TimedApplicationEventListenerTest {
     registry.clear();
   }
 
-  @Test
-  public void testMetricsEmittedOnSuccessfulRequest() {
+  @ParameterizedTest
+  @ValueSource(strings = {"%s/metrics", "%s/q/metrics"})
+  public void testMetricsEmittedOnSuccessfulRequest(String endpoint) {
     sendSuccessfulRequest();
     Map<String, MetricFamily> allMetrics =
-        TestMetricsUtil.fetchMetrics(fixture.client, testEnv.baseManagementUri());
+        TestMetricsUtil.fetchMetrics(fixture.client, testEnv.baseManagementUri(), endpoint);
     assertThat(allMetrics).containsKey(METRIC_NAME);
     assertThat(allMetrics.get(METRIC_NAME).getMetrics())
         .satisfiesOnlyOnce(
@@ -105,11 +107,12 @@ public class TimedApplicationEventListenerTest {
             });
   }
 
-  @Test
-  public void testMetricsEmittedOnFailedRequest() {
+  @ParameterizedTest
+  @ValueSource(strings = {"%s/metrics", "%s/q/metrics"})
+  public void testMetricsEmittedOnFailedRequest(String endpoint) {
     sendFailingRequest();
     Map<String, MetricFamily> allMetrics =
-        TestMetricsUtil.fetchMetrics(fixture.client, testEnv.baseManagementUri());
+        TestMetricsUtil.fetchMetrics(fixture.client, testEnv.baseManagementUri(), endpoint);
     assertThat(allMetrics).containsKey(METRIC_NAME);
     assertThat(allMetrics.get(METRIC_NAME).getMetrics())
         .satisfiesOnlyOnce(
