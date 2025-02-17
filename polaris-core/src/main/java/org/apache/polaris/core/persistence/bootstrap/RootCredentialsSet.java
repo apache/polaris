37a93ba7f688819197610daaafbcea0bb4977a9d
Eric Maynard <eric.maynard+oss@snowflake.com>
2025-02-17 11:24:47 -0800

    autolint

--- polaris-core/src/main/java/org/apache/polaris/core/persistence/bootstrap/RootCredentialsSet.java
+++ polaris-core/src/main/java/org/apache/polaris/core/persistence/bootstrap/RootCredentialsSet.java
@@ -34,7 +34,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.apache.polaris.core.entity.PolarisEntityConstants;
 import org.apache.polaris.immutables.PolarisImmutable;
 import org.immutables.value.Value;
