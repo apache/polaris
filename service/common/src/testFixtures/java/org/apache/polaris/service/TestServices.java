aac3ebbd76159d59f188ff4ae657d858d5a2294e
Eric Maynard <eric.maynard+oss@snowflake.com>
2025-02-20 20:57:39 -0800

    Revert "autolint"
    This reverts commit 66a7f22510582f3ebf71df7074d0d181ce26d2ac.

--- service/common/src/testFixtures/java/org/apache/polaris/service/TestServices.java
+++ service/common/src/testFixtures/java/org/apache/polaris/service/TestServices.java
@@ -28,6 +28,7 @@ import java.time.ZoneId;
 import java.util.Date;
 import java.util.Map;
 import java.util.Set;
+
 import org.apache.polaris.core.PolarisCallContext;
 import org.apache.polaris.core.PolarisConfigurationStore;
 import org.apache.polaris.core.PolarisDiagnostics;
