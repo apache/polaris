c20708eac3550dab5b023644372fedf17bc558cc
andrew4699 <andrew.guterman1@gmail.com>
2025-02-26 11:00:11 -0800

    Update default to 2

--- polaris-core/src/main/java/org/apache/polaris/core/PolarisConfiguration.java
+++ polaris-core/src/main/java/org/apache/polaris/core/PolarisConfiguration.java
@@ -265,12 +265,11 @@ public class PolarisConfiguration<T> {
           .defaultValue(30 * 60) // 30 minutes
           .build();
 
-  // The default value is kept at 12 for backward compatibility
   public static final PolarisConfiguration<Integer> MAX_METADATA_REFRESH_RETRIES =
       PolarisConfiguration.<Integer>builder()
           .key("MAX_METADATA_REFRESH_RETRIES")
           .description(
               "How many times to retry refreshing metadata when the previous error was retryable")
-          .defaultValue(12)
+          .defaultValue(2)
           .build();
 }
