412180c4727aedccd1ae333b1a25233bbc7369ba
Eric Maynard <eric.maynard+oss@snowflake.com>
2025-03-03 15:42:22 -0800

    add support for 01

--- polaris-core/src/main/java/org/apache/polaris/core/PolarisConfiguration.java
+++ polaris-core/src/main/java/org/apache/polaris/core/PolarisConfiguration.java
@@ -276,7 +276,9 @@ public class PolarisConfiguration<T> {
   public static final PolarisConfiguration<Integer> STORAGE_CONFIGURATION_MAX_LOCATIONS =
       PolarisConfiguration.<Integer>builder()
           .key("STORAGE_CONFIGURATION_MAX_LOCATIONS")
-          .description("How many locations can be associated with a storage configuration")
+          .catalogConfig("max.locations")
+          .description("How many locations can be associated with a storage configuration, or -1 for" +
+              " unlimited locations")
           .defaultValue(20)
           .build();
 }
