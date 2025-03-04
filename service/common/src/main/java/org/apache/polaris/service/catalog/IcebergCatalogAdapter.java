54e0824026eecf4f520d8a93c7929bdbfd55dd02
Eric Maynard <eric.maynard+oss@snowflake.com>
2025-03-03 19:48:07 -0800

    autolint

--- service/common/src/main/java/org/apache/polaris/service/catalog/IcebergCatalogAdapter.java
+++ service/common/src/main/java/org/apache/polaris/service/catalog/IcebergCatalogAdapter.java
@@ -35,9 +35,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
-
-import org.apache.iceberg.Snapshot;
-import org.apache.iceberg.SnapshotParser;
 import org.apache.iceberg.catalog.Catalog;
 import org.apache.iceberg.catalog.Namespace;
 import org.apache.iceberg.catalog.TableIdentifier;
