59997e06410ea05495eabcc1b7ec2f0c52f0c940
Yong <yongzheng0809@gmail.com>
2025-03-05 23:26:10 -0600

    Refactor

--- regtests/client/python/cli/polaris_cli.py
+++ regtests/client/python/cli/polaris_cli.py
@@ -143,25 +143,21 @@ class PolarisCli:
             polaris_management_url = f'http://{host}:{port}/api/management/v1'
             polaris_catalog_url = f'http://{host}:{port}/api/catalog/v1'
 
-        builder = None
-        config = Configuration(host=polaris_management_url) 
-        if options.proxy:
-            config.proxy = options.proxy
+        config = Configuration(host=polaris_management_url)
+        config.proxy = options.proxy
         if has_access_token:
             config.access_token = options.access_token
-            builder = lambda: ApiClient(config)
         elif has_client_secret:
             config.username = client_id
             config.password = client_secret
-            builder = lambda: ApiClient(config)
 
         if not has_access_token and not PolarisCli.DIRECT_AUTHENTICATION_ENABLED:
-            token = PolarisCli._get_token(builder(), polaris_catalog_url, client_id, client_secret)
+            token = PolarisCli._get_token(ApiClient(config), polaris_catalog_url, client_id, client_secret)
             config.username = None
             config.password = None
             config.access_token = token
-            builder = lambda: ApiClient(config)
-        return builder
+
+        return lambda: ApiClient(config)
 
 
 if __name__ == '__main__':
