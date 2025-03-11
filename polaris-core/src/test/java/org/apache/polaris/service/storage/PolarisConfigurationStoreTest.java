ec5894cebf04a5f6ad5e18d0e68fdd9e49c19da4
Eric Maynard <eric.maynard+oss@snowflake.com>
2025-03-10 22:23:23 -0700

    autolint

--- polaris-core/src/test/java/org/apache/polaris/service/storage/PolarisConfigurationStoreTest.java
+++ polaris-core/src/test/java/org/apache/polaris/service/storage/PolarisConfigurationStoreTest.java
@@ -21,7 +21,6 @@ package org.apache.polaris.service.storage;
 import jakarta.annotation.Nullable;
 import java.util.List;
 import java.util.function.Supplier;
-
 import org.apache.polaris.core.PolarisCallContext;
 import org.apache.polaris.core.config.BehaviorChangeConfiguration;
 import org.apache.polaris.core.config.FeatureConfiguration;
@@ -100,45 +99,45 @@ public class PolarisConfigurationStoreTest {
 
   private static class PolarisConfigurationConsumer {
 
-      private final PolarisCallContext polarisCallContext;
-      private final PolarisConfigurationStore configurationStore;
+    private final PolarisCallContext polarisCallContext;
+    private final PolarisConfigurationStore configurationStore;
 
-      public PolarisConfigurationConsumer(
-              PolarisCallContext polarisCallContext,
-              PolarisConfigurationStore configurationStore) {
-          this.polarisCallContext = polarisCallContext;
-          this.configurationStore = configurationStore;
-      }
+    public PolarisConfigurationConsumer(
+        PolarisCallContext polarisCallContext, PolarisConfigurationStore configurationStore) {
+      this.polarisCallContext = polarisCallContext;
+      this.configurationStore = configurationStore;
+    }
 
-      public <T> T consumeConfiguration(PolarisConfiguration<Boolean> config, Supplier<T> code, T defaultVal) {
-          if (configurationStore.getConfiguration(polarisCallContext, config)) {
-              return code.get();
-          }
-          return defaultVal;
+    public <T> T consumeConfiguration(
+        PolarisConfiguration<Boolean> config, Supplier<T> code, T defaultVal) {
+      if (configurationStore.getConfiguration(polarisCallContext, config)) {
+        return code.get();
       }
+      return defaultVal;
+    }
   }
 
   @Test
   public void testBehaviorAndFeatureConfigs() {
-      PolarisConfigurationConsumer consumer =
-          new PolarisConfigurationConsumer(null, new PolarisConfigurationStore() {});
+    PolarisConfigurationConsumer consumer =
+        new PolarisConfigurationConsumer(null, new PolarisConfigurationStore() {});
 
-      FeatureConfiguration<Boolean> featureConfig =
-          PolarisConfiguration.<Boolean>builder()
-              .key("example")
-              .description("example")
-              .defaultValue(true)
-              .buildFeatureConfiguration();
+    FeatureConfiguration<Boolean> featureConfig =
+        PolarisConfiguration.<Boolean>builder()
+            .key("example")
+            .description("example")
+            .defaultValue(true)
+            .buildFeatureConfiguration();
 
-      BehaviorChangeConfiguration<Boolean> behaviorChangeConfig =
-          PolarisConfiguration.<Boolean>builder()
-              .key("example")
-              .description("example")
-              .defaultValue(true)
-              .buildBehaviorChangeConfiguration();
+    BehaviorChangeConfiguration<Boolean> behaviorChangeConfig =
+        PolarisConfiguration.<Boolean>builder()
+            .key("example")
+            .description("example")
+            .defaultValue(true)
+            .buildBehaviorChangeConfiguration();
 
-      consumer.consumeConfiguration(behaviorChangeConfig, () -> 21, 22);
+    consumer.consumeConfiguration(behaviorChangeConfig, () -> 21, 22);
 
-      consumer.consumeConfiguration(featureConfig, () -> 42, 43);
+    consumer.consumeConfiguration(featureConfig, () -> 42, 43);
   }
 }
