72e2bb7e9b2865ce6fbb273f1cba8a439c46d91a
ashokborra <ashok.borra@fivetran.com>
2025-03-11 11:33:34 +0530

    Reset branch to empty state

--- .buildkite/scripts/build-polaris.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Placeholder script to build and validate Fivetran's fork of Polaris
-# This script is called from the main pipeline job
-# Add commands below as needed
-echo "Placeholder script"
-
