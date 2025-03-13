51f5e4537172874120f66b639b8e9029231c20d3
Eric Maynard <eric.maynard+oss@snowflake.com>
2025-03-13 11:31:51 -0700

    Adjustable limit on the number of locations per storage config (#1068)
    * initial commit
    
    * autolint
    
    * change the config store access
    
    * autolint
    
    * add support for 01
    
    * autolint
    
    * fix test
    
    * autolint
    
    * retest
    
    * rebase
    
    * autolint
    
    * change the config store access
    
    * autolint
    
    * add support for 01
    
    * autolint
    
    * fix test
    
    * autolint
    
    * retest
    
    * fix a test
    
    * autolint
    
    * fix another test
    
    * autolint
    
    * remove catalog config for now as it's not used
    
    * changes
    
    * autolint
    
    * update test to reflect -1 default
    
    * autolint
    
    * autolint
    
    * move the check
    
    * changes per review
    
    * ready
    
    * autolint

--- polaris-core/src/main/java/org/apache/polaris/core/entity/CatalogEntity.java
+++ polaris-core/src/main/java/org/apache/polaris/core/entity/CatalogEntity.java
@@ -23,6 +23,7 @@ import static org.apache.polaris.core.admin.model.StorageConfigInfo.StorageTypeE
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -39,6 +40,7 @@ import org.apache.polaris.core.admin.model.FileStorageConfigInfo;
 import org.apache.polaris.core.admin.model.GcpStorageConfigInfo;
 import org.apache.polaris.core.admin.model.PolarisCatalog;
 import org.apache.polaris.core.admin.model.StorageConfigInfo;
+import org.apache.polaris.core.config.BehaviorChangeConfiguration;
 import org.apache.polaris.core.storage.FileStorageConfigurationInfo;
 import org.apache.polaris.core.storage.PolarisStorageConfigurationInfo;
 import org.apache.polaris.core.storage.aws.AwsStorageConfigurationInfo;
@@ -237,6 +239,7 @@ public class CatalogEntity extends PolarisEntity {
           throw new BadRequestException("Must specify default base location");
         }
         allowedLocations.add(defaultBaseLocation);
+        validateMaxAllowedLocations(allowedLocations);
         switch (storageConfigModel.getStorageType()) {
           case S3:
             AwsStorageConfigInfo awsConfigModel = (AwsStorageConfigInfo) storageConfigModel;
@@ -277,6 +280,19 @@ public class CatalogEntity extends PolarisEntity {
             PolarisEntityConstants.getStorageConfigInfoPropertyName(), config.serialize());
       }
       return this;
+    }
+
+    /** Validate the number of allowed locations not exceeding the max value. */
+    private void validateMaxAllowedLocations(Collection<String> allowedLocations) {
+      int maxAllowedLocations =
+          BehaviorChangeConfiguration.loadConfig(
+              BehaviorChangeConfiguration.STORAGE_CONFIGURATION_MAX_LOCATIONS);
+      if (maxAllowedLocations != -1 && allowedLocations.size() > maxAllowedLocations) {
+        throw new IllegalArgumentException(
+            String.format(
+                "Number of configured locations (%s) exceeds the limit of %s",
+                allowedLocations.size(), maxAllowedLocations));
+      }
     }
 
     @Override
