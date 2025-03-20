57c389e4ca40b99a4244c9873d8bd4ce92c8402a
Liam Bao <liam.zw.bao@gmail.com>
2025-03-20 16:48:21 -0400

    Revert changes in IcebergHelper

--- integration-tests/src/main/java/org/apache/polaris/service/it/test/PolarisRestCatalogIntegrationTest.java
+++ integration-tests/src/main/java/org/apache/polaris/service/it/test/PolarisRestCatalogIntegrationTest.java
@@ -255,7 +255,6 @@ public class PolarisRestCatalogIntegrationTest extends CatalogTests<RESTCatalog>
         client,
         endpoints,
         principalCredentials,
-        catalogName,
         currentCatalogName,
         extraPropertiesBuilder.buildKeepingLast());
   }
