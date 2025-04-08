f2eb5c51f6441fa0e61ead2c061ed15716bb40fe
Liam Bao <liam.zw.bao@gmail.com>
2025-04-07 20:05:09 -0400

    Revert adding properties

--- service/common/src/main/java/org/apache/polaris/service/catalog/iceberg/IcebergCatalog.java
+++ service/common/src/main/java/org/apache/polaris/service/catalog/iceberg/IcebergCatalog.java
@@ -1277,15 +1277,13 @@ public class IcebergCatalog extends BaseMetastoreViewCatalog
               ? resolvedEntityView.getResolvedPath(tableIdentifier.namespace())
               : resolvedTableEntities;
 
-      Map<String, String> tableProperties = new HashMap<>(metadata.properties());
-
       // refresh credentials because we need to read the metadata file to validate its location
       tableFileIO =
           loadFileIOForTableLike(
               tableIdentifier,
               getLocationsAllowedToBeAccessed(metadata),
               resolvedStorageEntity,
-              tableProperties,
+              new HashMap<>(metadata.properties()),
               Set.of(PolarisStorageActions.READ, PolarisStorageActions.WRITE));
 
       List<PolarisEntity> resolvedNamespace =
@@ -1371,7 +1369,6 @@ public class IcebergCatalog extends BaseMetastoreViewCatalog
                 .setBaseLocation(metadata.location())
                 .setId(
                     getMetaStoreManager().generateNewEntityId(getCurrentPolarisContext()).getId())
-                .setProperties(tableProperties)
                 .build();
       } else {
         existingLocation = entity.getMetadataLocation();
@@ -1589,7 +1586,6 @@ public class IcebergCatalog extends BaseMetastoreViewCatalog
                 .setSubType(PolarisEntitySubType.ICEBERG_VIEW)
                 .setId(
                     getMetaStoreManager().generateNewEntityId(getCurrentPolarisContext()).getId())
-                .setProperties(tableProperties)
                 .build();
       } else {
         existingLocation = entity.getMetadataLocation();
