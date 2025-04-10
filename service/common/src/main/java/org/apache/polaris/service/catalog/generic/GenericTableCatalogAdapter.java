406c71074bd05b20f68521924d686fe2fe0f4ccf
Yun Zou <yunzou.colostate@gmail.com>
2025-04-10 11:49:15 -0700

    revert chagnes

--- service/common/src/main/java/org/apache/polaris/service/catalog/generic/GenericTableCatalogAdapter.java
+++ service/common/src/main/java/org/apache/polaris/service/catalog/generic/GenericTableCatalogAdapter.java
@@ -19,78 +19,14 @@
 package org.apache.polaris.service.catalog.generic;
 
 import jakarta.enterprise.context.RequestScoped;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.SecurityContext;
-import java.net.URLEncoder;
-import java.nio.charset.Charset;
-import org.apache.iceberg.catalog.Namespace;
-import org.apache.iceberg.catalog.TableIdentifier;
-import org.apache.iceberg.exceptions.NotAuthorizedException;
-import org.apache.iceberg.rest.RESTUtil;
-import org.apache.polaris.core.auth.AuthenticatedPolarisPrincipal;
-import org.apache.polaris.core.auth.PolarisAuthorizer;
-import org.apache.polaris.core.context.CallContext;
 import org.apache.polaris.core.context.RealmContext;
-import org.apache.polaris.core.persistence.PolarisEntityManager;
-import org.apache.polaris.core.persistence.PolarisMetaStoreManager;
-import org.apache.polaris.service.catalog.CatalogPrefixParser;
 import org.apache.polaris.service.catalog.api.PolarisCatalogGenericTableApiService;
-import org.apache.polaris.service.context.CallContextCatalogFactory;
 import org.apache.polaris.service.types.CreateGenericTableRequest;
 
 @RequestScoped
 public class GenericTableCatalogAdapter implements PolarisCatalogGenericTableApiService {
-  private final RealmContext realmContext;
-  private final CallContext callContext;
-  private final CallContextCatalogFactory catalogFactory;
-  private final PolarisEntityManager entityManager;
-  private final PolarisMetaStoreManager metaStoreManager;
-  private final PolarisAuthorizer polarisAuthorizer;
-  private final CatalogPrefixParser prefixParser;
-
-  @Inject
-  public GenericTableCatalogAdapter(
-      RealmContext realmContext,
-      CallContext callContext,
-      CallContextCatalogFactory catalogFactory,
-      PolarisEntityManager entityManager,
-      PolarisMetaStoreManager metaStoreManager,
-      PolarisAuthorizer polarisAuthorizer,
-      CatalogPrefixParser prefixParser) {
-    this.realmContext = realmContext;
-    this.callContext = callContext;
-    this.catalogFactory = catalogFactory;
-    this.entityManager = entityManager;
-    this.metaStoreManager = metaStoreManager;
-    this.polarisAuthorizer = polarisAuthorizer;
-    this.prefixParser = prefixParser;
-
-    // FIXME: This is a hack to set the current context for downstream calls.
-    CallContext.setCurrentContext(callContext);
-  }
-
-  private GenericTableCatalogHandler newHandlerWrapper(
-      SecurityContext securityContext, String catalogName) {
-    AuthenticatedPolarisPrincipal authenticatedPrincipal =
-        (AuthenticatedPolarisPrincipal) securityContext.getUserPrincipal();
-    if (authenticatedPrincipal == null) {
-      throw new NotAuthorizedException("Failed to find authenticatedPrincipal in SecurityContext");
-    }
-
-    return new GenericTableCatalogHandler(
-        callContext,
-        entityManager,
-        metaStoreManager,
-        securityContext,
-        catalogName,
-        polarisAuthorizer);
-  }
-
-  private static Namespace decodeNamespace(String namespace) {
-    return RESTUtil.decodeNamespace(URLEncoder.encode(namespace, Charset.defaultCharset()));
-  }
-
   @Override
   public Response createGenericTable(
       String prefix,
@@ -98,16 +34,7 @@ public class GenericTableCatalogAdapter implements PolarisCatalogGenericTableApi
       CreateGenericTableRequest createGenericTableRequest,
       RealmContext realmContext,
       SecurityContext securityContext) {
-    Namespace ns = decodeNamespace(namespace);
-
-    return Response.ok(
-            newHandlerWrapper(securityContext, prefix)
-                .createGenericTable(
-                    TableIdentifier.of(ns, createGenericTableRequest.getName()),
-                    createGenericTableRequest.getFormat(),
-                    createGenericTableRequest.getDoc(),
-                    createGenericTableRequest.getProperties()))
-        .build();
+    return Response.status(501).build(); // not implemented
   }
 
   @Override
@@ -138,9 +65,6 @@ public class GenericTableCatalogAdapter implements PolarisCatalogGenericTableApi
       String genericTable,
       RealmContext realmContext,
       SecurityContext securityContext) {
-    Namespace ns = decodeNamespace(namespace);
-    TableIdentifier tableIdentifier = TableIdentifier.of(ns, RESTUtil.decodeString(genericTable));
-    return Response.ok(newHandlerWrapper(securityContext, prefix).loadGenericTable(tableIdentifier))
-        .build();
+    return Response.status(501).build(); // not implemented
   }
 }
