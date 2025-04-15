c48b8d24288afeaf95bf4b613bfa00b3a0e38c45
fivetran-arjuncnair <arjun.cnair@fivetran.com>
2025-04-15 11:15:25 +0530

    Adding buildkite Pipeline scripts

---
+++ .buildkite/scripts/build-polaris.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Placeholder script to build and validate Fivetran's fork of Polaris
+# This script is called from the main pipeline job
+# Add commands below as needed
+echo "Placeholder script"
+
