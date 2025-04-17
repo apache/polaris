e359420dc829712c546a76986b80d9a898976707
gh-yzou <167037035+gh-yzou@users.noreply.github.com>
2025-04-17 09:55:24 -0700

    Integration test for Spark Client (#1349)
    * add integration test
    
    * add change
    
    * add comments
    
    * rebase main
    
    * update class comments
    
    * add base integration
    
    * clean up comments

--- plugins/spark/v3.5/spark/build.gradle.kts
+++ plugins/spark/v3.5/spark/build.gradle.kts
@@ -24,17 +24,6 @@ plugins {
   alias(libs.plugins.jandex)
 }
 
-fun getAndUseScalaVersionForProject(): String {
-  val sparkScala = project.name.split("-").last().split("_")
-
-  val scalaVersion = sparkScala[1]
-
-  // direct the build to build/<scalaVersion> to avoid potential collision problem
-  project.layout.buildDirectory.set(layout.buildDirectory.dir(scalaVersion).get())
-
-  return scalaVersion
-}
-
 // get version information
 val sparkMajorVersion = "3.5"
 val scalaVersion = getAndUseScalaVersionForProject()
@@ -141,6 +130,7 @@ tasks.register<ShadowJar>("createPolarisSparkJar") {
   mergeServiceFiles()
 
   // pack both the source code and dependencies
+
   from(sourceSets.main.get().output)
   configurations = listOf(project.configurations.runtimeClasspath.get())
 
