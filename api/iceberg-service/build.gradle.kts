5c26ec6e244d44ea56b2992b56e6158fbdd3386d
Eric Maynard <eric.maynard+oss@snowflake.com>
2025-04-23 11:09:01 -0700

    example

--- api/iceberg-service/build.gradle.kts
+++ api/iceberg-service/build.gradle.kts
@@ -118,3 +118,7 @@ sourceSets {
 tasks.named("javadoc") { dependsOn("jandex") }
 
 tasks.named("processResources") { dependsOn("openApiGenerate") }
+
+tasks.named("openApiGenerate") {
+  outputs.cacheIf { false }
+}
