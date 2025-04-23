50587eab77edd796cf042e50f3d5f31b665f57ea
adnanhemani <adnan.h@berkeley.edu>
2025-04-23 16:27:35 -0700

    debug

--- getting-started/assets/cloud_providers/deploy-aws.sh
+++ getting-started/assets/cloud_providers/deploy-aws.sh
@@ -78,7 +78,8 @@ aws s3api create-bucket --bucket $S3_BUCKET_NAME --region $CURRENT_REGION --crea
 
 export STORAGE_LOCATION="s3://$S3_BUCKET_NAME/quickstart_catalog/"
 
-export SPARK_ADDITIONAL_JARS=",org.apache.hadoop:hadoop-azure:3.3.4,org.apache.hadoop:hadoop-azure-datalake:3.3.4,org.apache.iceberg:iceberg-azure-bundle:1.6.1"
+SPARK_ADDITIONAL_JARS=",software.amazon.awssdk:bundle:2.28.17,software.amazon.awssdk:url-connection-client:2.28.17"
+sed "/^\s*--packages/s|\",$|${SPARK_ADDITIONAL_JARS}\",|" getting-started/eclipselink/docker-compose.yml
 
 ./gradlew clean :polaris-quarkus-server:assemble :polaris-quarkus-admin:assemble \
        -PeclipseLinkDeps=org.postgresql:postgresql:42.7.4 \
--- getting-started/assets/cloud_providers/deploy-azure.sh
+++ getting-started/assets/cloud_providers/deploy-azure.sh
@@ -65,7 +65,8 @@ fs.native-azure.enabled=true
 azure.auth-type=DEFAULT
 EOF
 
-export SPARK_ADDITIONAL_JARS=",software.amazon.awssdk:bundle:2.28.17,software.amazon.awssdk:url-connection-client:2.28.17"
+SPARK_ADDITIONAL_JARS=",org.apache.hadoop:hadoop-azure:3.3.4,org.apache.hadoop:hadoop-azure-datalake:3.3.4,org.apache.iceberg:iceberg-azure-bundle:1.6.1"
+sed "/^\s*--packages/s|\",$|${SPARK_ADDITIONAL_JARS}\",|" getting-started/eclipselink/docker-compose.yml
 
 ./gradlew clean :polaris-quarkus-server:assemble :polaris-quarkus-admin:assemble \
        -PeclipseLinkDeps=org.postgresql:postgresql:42.7.4 \
