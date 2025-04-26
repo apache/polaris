9bf2972e97df71ea2cf05af419e8bbdf6066216a
Prashant <singhpk010696@gmail.com>
2025-04-25 18:24:28 -0700

    remove the exlusion

--- quarkus/server/build.gradle.kts
+++ quarkus/server/build.gradle.kts
@@ -51,11 +51,7 @@ dependencies {
   runtimeOnly(project(":polaris-eclipselink"))
   runtimeOnly("org.postgresql:postgresql")
   runtimeOnly(project(":polaris-relational-jdbc"))
-  runtimeOnly("io.quarkus:quarkus-jdbc-postgresql") {
-    exclude(group = "org.antlr", module = "antlr4-runtime")
-    exclude(group = "org.scala-lang", module = "scala-library")
-    exclude(group = "org.scala-lang", module = "scala-reflect")
-  }
+  runtimeOnly("io.quarkus:quarkus-jdbc-postgresql")
 
   // enforce the Quarkus _platform_ here, to get a consistent and validated set of dependencies
   implementation(enforcedPlatform(libs.quarkus.bom))
