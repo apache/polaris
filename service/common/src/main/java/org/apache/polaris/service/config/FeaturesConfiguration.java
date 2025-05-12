c03b04d6594818c2dbcfb3cf687f1ebbe6ba8b32
Eric Maynard <eric.maynard+oss@snowflake.com>
2025-05-12 11:13:50 -0700

    autolint

--- service/common/src/main/java/org/apache/polaris/service/config/FeaturesConfiguration.java
+++ service/common/src/main/java/org/apache/polaris/service/config/FeaturesConfiguration.java
@@ -21,7 +21,6 @@ package org.apache.polaris.service.config;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.smallrye.config.WithParentName;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
