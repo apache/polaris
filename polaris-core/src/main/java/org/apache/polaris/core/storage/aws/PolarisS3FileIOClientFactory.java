98e6f6ac02d448cae937b61b6ddcd0be612daf9d
ashok.borra <ashok.borra@fivetran.com>
2025-05-13 17:51:52 +0530

    tweak(Polaris): Fix formatting

--- polaris-core/src/main/java/org/apache/polaris/core/storage/aws/PolarisS3FileIOClientFactory.java
+++ polaris-core/src/main/java/org/apache/polaris/core/storage/aws/PolarisS3FileIOClientFactory.java
@@ -18,13 +18,12 @@
  */
 package org.apache.polaris.core.storage.aws;
 
+import java.util.Map;
 import org.apache.iceberg.aws.AwsClientProperties;
 import org.apache.iceberg.aws.HttpClientProperties;
 import org.apache.iceberg.aws.s3.S3FileIOAwsClientFactory;
 import org.apache.iceberg.aws.s3.S3FileIOProperties;
 import software.amazon.awssdk.services.s3.S3Client;
-
-import java.util.Map;
 
 /**
  * A S3FileIOAwsClientFactory that will be used by the S3FileIO to initialize S3 client. The
@@ -33,37 +32,37 @@ import java.util.Map;
  * href="https://github.com/apache/iceberg/issues/9785">here</a>
  */
 public class PolarisS3FileIOClientFactory implements S3FileIOAwsClientFactory {
-    private S3FileIOProperties s3FileIOProperties;
-    private HttpClientProperties httpClientProperties;
-    private AwsClientProperties awsClientProperties;
+  private S3FileIOProperties s3FileIOProperties;
+  private HttpClientProperties httpClientProperties;
+  private AwsClientProperties awsClientProperties;
 
-    PolarisS3FileIOClientFactory() {
-        this.s3FileIOProperties = new S3FileIOProperties();
-        this.httpClientProperties = new HttpClientProperties();
-        this.awsClientProperties = new AwsClientProperties();
-    }
+  PolarisS3FileIOClientFactory() {
+    this.s3FileIOProperties = new S3FileIOProperties();
+    this.httpClientProperties = new HttpClientProperties();
+    this.awsClientProperties = new AwsClientProperties();
+  }
 
-    @Override
-    public void initialize(Map<String, String> properties) {
-        this.s3FileIOProperties = new S3FileIOProperties(properties);
-        this.awsClientProperties = new AwsClientProperties(properties);
-        this.httpClientProperties = new HttpClientProperties(properties);
-    }
+  @Override
+  public void initialize(Map<String, String> properties) {
+    this.s3FileIOProperties = new S3FileIOProperties(properties);
+    this.awsClientProperties = new AwsClientProperties(properties);
+    this.httpClientProperties = new HttpClientProperties(properties);
+  }
 
-    @Override
-    public S3Client s3() {
-        return S3Client.builder()
-                .applyMutation(awsClientProperties::applyClientRegionConfiguration)
-                .applyMutation(httpClientProperties::applyHttpClientConfigurations)
-                .applyMutation(s3FileIOProperties::applyEndpointConfigurations)
-                .applyMutation(s3FileIOProperties::applyServiceConfigurations)
-                .applyMutation(
-                        s3ClientBuilder ->
-                                s3FileIOProperties.applyCredentialConfigurations(
-                                        awsClientProperties, s3ClientBuilder))
-                .applyMutation(s3FileIOProperties::applySignerConfiguration)
-                .applyMutation(s3FileIOProperties::applyS3AccessGrantsConfigurations)
-                .applyMutation(s3ClientBuilder -> s3ClientBuilder.crossRegionAccessEnabled(true))
-                .build();
-    }
+  @Override
+  public S3Client s3() {
+    return S3Client.builder()
+        .applyMutation(awsClientProperties::applyClientRegionConfiguration)
+        .applyMutation(httpClientProperties::applyHttpClientConfigurations)
+        .applyMutation(s3FileIOProperties::applyEndpointConfigurations)
+        .applyMutation(s3FileIOProperties::applyServiceConfigurations)
+        .applyMutation(
+            s3ClientBuilder ->
+                s3FileIOProperties.applyCredentialConfigurations(
+                    awsClientProperties, s3ClientBuilder))
+        .applyMutation(s3FileIOProperties::applySignerConfiguration)
+        .applyMutation(s3FileIOProperties::applyS3AccessGrantsConfigurations)
+        .applyMutation(s3ClientBuilder -> s3ClientBuilder.crossRegionAccessEnabled(true))
+        .build();
+  }
 }
