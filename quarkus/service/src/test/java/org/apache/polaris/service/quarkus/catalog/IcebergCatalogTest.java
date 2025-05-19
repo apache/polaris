b270a842c6d5d009708daf8c04d4a03c36e7b706
Prashant <singhpk010696@gmail.com>
2025-05-19 08:31:15 -0700

    Address review feedback

--- quarkus/service/src/test/java/org/apache/polaris/service/quarkus/catalog/IcebergCatalogTest.java
+++ quarkus/service/src/test/java/org/apache/polaris/service/quarkus/catalog/IcebergCatalogTest.java
@@ -625,7 +625,7 @@ public abstract class IcebergCatalogTest extends CatalogTests<IcebergCatalog> {
         .newRewrite()
         .addFile(FILE_B)
         .deleteFile(FILE_A)
-        .set("polaris.rollback.compaction.on-conflict", "true")
+        .set("polaris.conflict-resolution.by-operation-type.replace", "rollback")
         .commit();
 
     try {
@@ -736,7 +736,7 @@ public abstract class IcebergCatalogTest extends CatalogTests<IcebergCatalog> {
         .newRewrite()
         .addFile(FILE_B)
         .deleteFile(FILE_A)
-        .set("polaris.rollback.compaction.on-conflict", "true")
+        .set("polaris.conflict-resolution.by-operation-type.replace", "rollback")
         .commit();
 
     // commit FILE_C
@@ -810,7 +810,7 @@ public abstract class IcebergCatalogTest extends CatalogTests<IcebergCatalog> {
         .newRewrite()
         .addFile(FILE_B)
         .deleteFile(FILE_A)
-        .set("polaris.rollback.compaction.on-conflict", "true")
+        .set("polaris.conflict-resolution.by-operation-type.replace", "rollback")
         .commit();
 
     Table t = catalog.loadTable(TABLE);
