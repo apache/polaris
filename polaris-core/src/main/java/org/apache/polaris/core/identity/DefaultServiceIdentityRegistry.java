c8fe1b05e0817a70bd4736dcd2ba5b0be1ffc539
Rulin Xing <rulin.xing+oss@snowflake.com>
2025-05-27 17:07:18 -0700

    Add Service Identity Registry

---
+++ polaris-core/src/main/java/org/apache/polaris/core/identity/DefaultServiceIdentityRegistry.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.polaris.core.identity;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.polaris.core.identity.dpo.ServiceIdentityInfoDpo;
+import org.apache.polaris.core.identity.resolved.ResolvedServiceIdentity;
+
+public class DefaultServiceIdentityRegistry implements ServiceIdentityRegistry {
+
+  private final EnumMap<ServiceIdentityType, ResolvedServiceIdentity> resolvedServiceIdentities;
+  private final Map<String, ResolvedServiceIdentity> referenceToResolvedServiceIdentity;
+
+  public DefaultServiceIdentityRegistry(
+      EnumMap<ServiceIdentityType, ResolvedServiceIdentity> serviceIdentities) {
+    this.resolvedServiceIdentities = serviceIdentities;
+    this.referenceToResolvedServiceIdentity =
+        serviceIdentities.values().stream()
+            .collect(
+                Collectors.toMap(
+                    identity -> identity.getIdentityInfoReference().getUrn(),
+                    identity -> identity));
+  }
+
+  @Override
+  public ServiceIdentityInfoDpo assignServiceIdentity(ServiceIdentityType serviceIdentityType) {
+    ResolvedServiceIdentity resolvedServiceIdentity =
+        resolvedServiceIdentities.get(serviceIdentityType);
+    if (resolvedServiceIdentity == null) {
+      throw new IllegalArgumentException(
+          "Service identity type not supported: " + serviceIdentityType);
+    }
+    return resolvedServiceIdentity.asServiceIdentityInfoDpo();
+  }
+
+  @Override
+  public ResolvedServiceIdentity resolveServiceIdentity(
+      ServiceIdentityInfoDpo serviceIdentityInfo) {
+    ResolvedServiceIdentity resolvedServiceIdentity =
+        referenceToResolvedServiceIdentity.get(
+            serviceIdentityInfo.getIdentityInfoReference().getUrn());
+    return resolvedServiceIdentity;
+  }
+}
---
+++ polaris-core/src/main/java/org/apache/polaris/core/identity/resolved/ResolvedAwsIamServiceIdentity.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.polaris.core.identity.resolved;
+
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import jakarta.annotation.Nonnull;
+import org.apache.polaris.core.identity.ServiceIdentityType;
+import org.apache.polaris.core.identity.dpo.AwsIamServiceIdentityInfoDpo;
+import org.apache.polaris.core.identity.dpo.ServiceIdentityInfoDpo;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.StsClientBuilder;
+
+public class ResolvedAwsIamServiceIdentity extends ResolvedServiceIdentity {
+  private final String iamArn;
+  private final String accessKeyId;
+  private final String secretAccessKey;
+  private final String sessionToken;
+
+  public ResolvedAwsIamServiceIdentity(
+      String iamArn, String accessKeyId, String secretAccessKey, String sessionToken) {
+    super(ServiceIdentityType.AWS_IAM);
+    this.iamArn = iamArn;
+    this.accessKeyId = accessKeyId;
+    this.secretAccessKey = secretAccessKey;
+    this.sessionToken = sessionToken;
+  }
+
+  public String getIamArn() {
+    return iamArn;
+  }
+
+  public String getAccessKeyId() {
+    return accessKeyId;
+  }
+
+  public String getSecretAccessKey() {
+    return secretAccessKey;
+  }
+
+  public String getSessionToken() {
+    return sessionToken;
+  }
+
+  @Nonnull
+  @Override
+  public ServiceIdentityInfoDpo asServiceIdentityInfoDpo() {
+    return new AwsIamServiceIdentityInfoDpo(getIdentityInfoReference(), getIamArn());
+  }
+
+  public Supplier<StsClient> stsClientSupplier() {
+    return Suppliers.memoize(
+        () -> {
+          StsClientBuilder stsClientBuilder = StsClient.builder();
+          if (getAccessKeyId() != null && getSecretAccessKey() != null) {
+            StaticCredentialsProvider awsCredentialsProvider =
+                StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create(getAccessKeyId(), getSecretAccessKey()));
+            if (getSessionToken() != null) {
+              awsCredentialsProvider =
+                  StaticCredentialsProvider.create(
+                      AwsSessionCredentials.create(
+                          getAccessKeyId(), getSecretAccessKey(), getSessionToken()));
+            }
+            stsClientBuilder.credentialsProvider(awsCredentialsProvider);
+          }
+          return stsClientBuilder.build();
+        });
+  }
+}
