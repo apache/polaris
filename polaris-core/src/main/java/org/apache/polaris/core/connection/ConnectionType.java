cb6a6188930bb8c94b458b220f479108268f32d6
Pooja Nilangekar <poojan@umd.edu>
2025-06-06 19:02:59 -0700

    Apply spotless format changes

--- polaris-core/src/main/java/org/apache/polaris/core/connection/ConnectionType.java
+++ polaris-core/src/main/java/org/apache/polaris/core/connection/ConnectionType.java
@@ -40,10 +40,7 @@ public enum ConnectionType {
   static {
     // find max array size
     int maxCode =
-        Arrays.stream(ConnectionType.values())
-            .mapToInt(ConnectionType::getCode)
-            .max()
-            .orElse(0);
+        Arrays.stream(ConnectionType.values()).mapToInt(ConnectionType::getCode).max().orElse(0);
 
     // allocate mapping array
     REVERSE_MAPPING_ARRAY = new ConnectionType[maxCode + 1];
