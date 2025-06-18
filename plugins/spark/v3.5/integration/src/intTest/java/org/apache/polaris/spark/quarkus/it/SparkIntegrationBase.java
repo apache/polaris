fc7a60b1babdca5366e84dd559005fdd6cd34fe2
Yun Zou <yunzou.colostate@gmail.com>
2025-06-18 10:42:46 -0700

    add change

--- plugins/spark/v3.5/integration/src/intTest/java/org/apache/polaris/spark/quarkus/it/SparkIntegrationBase.java
+++ plugins/spark/v3.5/integration/src/intTest/java/org/apache/polaris/spark/quarkus/it/SparkIntegrationBase.java
@@ -18,23 +18,136 @@
  */
 package org.apache.polaris.spark.quarkus.it;
 
+import com.adobe.testing.s3mock.testcontainers.S3MockContainer;
 import com.google.common.collect.ImmutableList;
 import com.google.errorprone.annotations.FormatMethod;
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.apache.commons.io.filefilter.FalseFileFilter;
-import org.apache.polaris.service.it.ext.PolarisSparkIntegrationTestBase;
+import org.apache.polaris.core.admin.model.AwsStorageConfigInfo;
+import org.apache.polaris.core.admin.model.Catalog;
+import org.apache.polaris.core.admin.model.CatalogProperties;
+import org.apache.polaris.core.admin.model.PolarisCatalog;
+import org.apache.polaris.core.admin.model.StorageConfigInfo;
+import org.apache.polaris.service.it.env.CatalogApi;
+import org.apache.polaris.service.it.env.ClientCredentials;
+import org.apache.polaris.service.it.env.IntegrationTestsHelper;
+import org.apache.polaris.service.it.env.ManagementApi;
+import org.apache.polaris.service.it.env.PolarisApiEndpoints;
+import org.apache.polaris.service.it.ext.PolarisIntegrationTestExtension;
+import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
+import org.slf4j.LoggerFactory;
 
-public abstract class SparkIntegrationBase extends PolarisSparkIntegrationTestBase {
+@ExtendWith(PolarisIntegrationTestExtension.class)
+public abstract class SparkIntegrationBase {
+  protected static final S3MockContainer s3Container =
+      new S3MockContainer("3.11.0").withInitialBuckets("my-bucket,my-old-bucket");
+  protected static SparkSession spark;
+  protected PolarisApiEndpoints endpoints;
+  protected PolarisManagementClient client;
+  protected ManagementApi managementApi;
+  protected CatalogApi catalogApi;
+  protected String catalogName;
+  protected String sparkToken;
 
-  @Override
+  protected URI warehouseDir;
+
+  @BeforeAll
+  public static void setup() throws IOException {
+    s3Container.start();
+  }
+
+  @AfterAll
+  public static void cleanup() {
+    s3Container.stop();
+  }
+
+  @BeforeEach
+  public void before(
+      PolarisApiEndpoints apiEndpoints, ClientCredentials credentials, @TempDir Path tempDir) {
+    endpoints = apiEndpoints;
+    client = PolarisManagementClient.managementClient(endpoints);
+    sparkToken = client.obtainToken(credentials);
+    managementApi = client.managementApi(credentials);
+    catalogApi = client.catalogApi(credentials);
+
+    warehouseDir = IntegrationTestsHelper.getTemporaryDirectory(tempDir).resolve("spark-warehouse");
+
+    catalogName = client.newEntityName("spark_catalog");
+
+    AwsStorageConfigInfo awsConfigModel =
+        AwsStorageConfigInfo.builder()
+            .setRoleArn("arn:aws:iam::123456789012:role/my-role")
+            .setExternalId("externalId")
+            .setUserArn("userArn")
+            .setStorageType(StorageConfigInfo.StorageTypeEnum.S3)
+            .setAllowedLocations(List.of("s3://my-old-bucket/path/to/data"))
+            .build();
+    CatalogProperties props = new CatalogProperties("s3://my-bucket/path/to/data");
+    props.putAll(
+        Map.of(
+            "table-default.s3.endpoint",
+            s3Container.getHttpEndpoint(),
+            "table-default.s3.path-style-access",
+            "true",
+            "table-default.s3.access-key-id",
+            "foo",
+            "table-default.s3.secret-access-key",
+            "bar",
+            "s3.endpoint",
+            s3Container.getHttpEndpoint(),
+            "s3.path-style-access",
+            "true",
+            "s3.access-key-id",
+            "foo",
+            "s3.secret-access-key",
+            "bar",
+            "polaris.config.drop-with-purge.enabled",
+            "true"));
+    Catalog catalog =
+        PolarisCatalog.builder()
+            .setType(Catalog.TypeEnum.INTERNAL)
+            .setName(catalogName)
+            .setProperties(props)
+            .setStorageConfigInfo(awsConfigModel)
+            .build();
+
+    managementApi.createCatalog(catalog);
+
+    SparkSession.Builder sessionBuilder =
+        SparkSession.builder()
+            .master("local[1]")
+            .config("spark.hadoop.fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+            .config(
+                "spark.hadoop.fs.s3.aws.credentials.provider",
+                "org.apache.hadoop.fs.s3.TemporaryAWSCredentialsProvider")
+            .config("spark.hadoop.fs.s3.access.key", "foo")
+            .config("spark.hadoop.fs.s3.secret.key", "bar")
+            .config("spark.ui.showConsoleProgress", false)
+            .config("spark.ui.enabled", "false");
+    spark = withCatalog(sessionBuilder, catalogName).getOrCreate();
+
+    onSpark("USE " + catalogName);
+  }
+
   protected SparkSession.Builder withCatalog(SparkSession.Builder builder, String catalogName) {
     return builder
         .config(
@@ -59,6 +172,38 @@ public abstract class SparkIntegrationBase extends PolarisSparkIntegrationTestBa
         .config(
             String.format("spark.sql.catalog.%s.s3.secret-access-key", catalogName), "fakesecret")
         .config(String.format("spark.sql.catalog.%s.s3.region", catalogName), "us-west-2");
+  }
+
+  @AfterEach
+  public void after() throws Exception {
+    cleanupCatalog(catalogName);
+    try {
+      SparkSession.clearDefaultSession();
+      SparkSession.clearActiveSession();
+      spark.close();
+    } catch (Exception e) {
+      LoggerFactory.getLogger(getClass()).error("Unable to close spark session", e);
+    }
+
+    client.close();
+  }
+
+  protected void cleanupCatalog(String catalogName) {
+    onSpark("USE " + catalogName);
+    List<Row> namespaces = onSpark("SHOW NAMESPACES").collectAsList();
+    for (Row namespace : namespaces) {
+      List<Row> tables = onSpark("SHOW TABLES IN " + namespace.getString(0)).collectAsList();
+      for (Row table : tables) {
+        onSpark("DROP TABLE " + namespace.getString(0) + "." + table.getString(1));
+      }
+      List<Row> views = onSpark("SHOW VIEWS IN " + namespace.getString(0)).collectAsList();
+      for (Row view : views) {
+        onSpark("DROP VIEW " + namespace.getString(0) + "." + view.getString(1));
+      }
+      onSpark("DROP NAMESPACE " + namespace.getString(0));
+    }
+
+    managementApi.deleteCatalog(catalogName);
   }
 
   @FormatMethod
@@ -109,5 +254,9 @@ public abstract class SparkIntegrationBase extends PolarisSparkIntegrationTestBa
   /** Generate a string name with given prefix and a random suffix */
   protected String generateName(String prefix) {
     return prefix + "_" + UUID.randomUUID().toString().replaceAll("-", "");
+  }
+
+  protected static Dataset<Row> onSpark(@Language("SQL") String sql) {
+    return spark.sql(sql);
   }
 }
