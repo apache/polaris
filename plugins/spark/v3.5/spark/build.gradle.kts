bac7089b281c39ea564ad4c6d1d80ad09bd335d3
Rahil Chertara <rchertara@gmail.com>
2025-07-02 11:18:04 -0700

    cleanup dependencies

--- plugins/spark/v3.5/spark/build.gradle.kts
+++ plugins/spark/v3.5/spark/build.gradle.kts
@@ -46,46 +46,8 @@ dependencies {
   // TODO: extract a polaris-rest module as a thin layer for
   //  client to depends on.
   implementation(project(":polaris-core")) { isTransitive = false }
-  implementation(project(":polaris-api-iceberg-service")) {
-    // exclude the iceberg dependencies, use the ones pulled
-    // by iceberg-core
-    exclude("org.apache.iceberg", "*")
-    // exclude all cloud and quarkus specific dependencies to avoid
-    // running into problems with signature files.
-    exclude("com.azure", "*")
-    exclude("software.amazon.awssdk", "*")
-    exclude("com.google.cloud", "*")
-    exclude("io.airlift", "*")
-    exclude("io.smallrye", "*")
-    exclude("io.smallrye.common", "*")
-    exclude("io.swagger", "*")
-    exclude("org.apache.commons", "*")
-  }
-  implementation(project(":polaris-api-catalog-service")) {
-    exclude("org.apache.iceberg", "*")
-    exclude("com.azure", "*")
-    exclude("software.amazon.awssdk", "*")
-    exclude("com.google.cloud", "*")
-    exclude("io.airlift", "*")
-    exclude("io.smallrye", "*")
-    exclude("io.smallrye.common", "*")
-    exclude("io.swagger", "*")
-    exclude("org.apache.commons", "*")
-  }
-  implementation(project(":polaris-core")) {
-    exclude("org.apache.iceberg", "*")
-    exclude("com.azure", "*")
-    exclude("software.amazon.awssdk", "*")
-    exclude("com.google.cloud", "*")
-    exclude("io.airlift", "*")
-    exclude("io.smallrye", "*")
-    exclude("io.smallrye.common", "*")
-    exclude("io.swagger", "*")
-    exclude("org.apache.commons", "*")
-  }
-
-  implementation("org.apache.iceberg:iceberg-core:${icebergVersion}")
-  compileOnly("org.apache.hudi:hudi-spark3.5-bundle_${scalaVersion}:0.15.0")
+  //compileOnly("org.apache.hudi:hudi-spark3.5-bundle_${scalaVersion}:0.15.0")
+  compileOnly("org.apache.hudi:hudi-spark3.2plus-common:0.15.0")
   testImplementation("org.apache.hudi:hudi-spark3.5-bundle_${scalaVersion}:0.15.0")
 
   implementation(
