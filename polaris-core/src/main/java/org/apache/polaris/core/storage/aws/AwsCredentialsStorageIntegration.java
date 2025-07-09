2db692c28c45d820358a48d1d807a0d92165f4c0
Dmitri Bourlatchkov <dmitri.bourlatchkov@gmail.com>
2025-07-09 09:17:49 -0400

    review: rename to getPathStyleAccess

--- polaris-core/src/main/java/org/apache/polaris/core/storage/aws/AwsCredentialsStorageIntegration.java
+++ polaris-core/src/main/java/org/apache/polaris/core/storage/aws/AwsCredentialsStorageIntegration.java
@@ -122,7 +122,7 @@ public class AwsCredentialsStorageIntegration
       credentialMap.put(StorageAccessProperty.AWS_ENDPOINT, endpointUri.toString());
     }
 
-    if (storageConfig.forcePathStyleAccess()) {
+    if (storageConfig.getPathStyleAccess()) {
       credentialMap.put(StorageAccessProperty.AWS_PATH_STYLE_ACCESS, Boolean.TRUE.toString());
     }
 
