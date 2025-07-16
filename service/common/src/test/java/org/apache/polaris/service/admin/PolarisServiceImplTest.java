be811024a7e0fc926f8f96739c9fc433fba43282
adnanhemani <adnan.h@berkeley.edu>
2025-07-15 19:18:12 -0700

    merge from main

--- service/common/src/test/java/org/apache/polaris/service/admin/PolarisServiceImplTest.java
+++ service/common/src/test/java/org/apache/polaris/service/admin/PolarisServiceImplTest.java
@@ -40,6 +40,8 @@ import org.apache.polaris.core.persistence.MetaStoreManagerFactory;
 import org.apache.polaris.core.secrets.UserSecretsManagerFactory;
 import org.apache.polaris.service.config.RealmEntityManagerFactory;
 import org.apache.polaris.service.config.ReservedProperties;
+import org.apache.polaris.service.events.NoOpPolarisEventListener;
+import org.apache.polaris.service.events.PolarisEventListener;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -80,7 +82,8 @@ public class PolarisServiceImplTest {
             userSecretsManagerFactory,
             polarisAuthorizer,
             callContext,
-            reservedProperties);
+            reservedProperties,
+            new NoOpPolarisEventListener());
   }
 
   @Test
