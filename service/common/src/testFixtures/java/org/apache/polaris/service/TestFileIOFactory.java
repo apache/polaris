7ca49702694f0200752d9d7e41a2d7f319b23fea
Christopher Lambert <xn137@gmx.de>
2025-07-16 19:36:20 +0200

    Add TestFileIOFactory helper (#2105)
    * Add FileIOFactory.wrapExisting helper

---
+++ service/common/src/testFixtures/java/org/apache/polaris/service/TestFileIOFactory.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.polaris.service;
+
+import jakarta.annotation.Nonnull;
+import java.util.Map;
+import java.util.Set;
+import org.apache.iceberg.catalog.TableIdentifier;
+import org.apache.iceberg.io.FileIO;
+import org.apache.polaris.core.context.CallContext;
+import org.apache.polaris.core.persistence.PolarisResolvedPathWrapper;
+import org.apache.polaris.core.storage.PolarisStorageActions;
+import org.apache.polaris.service.catalog.io.FileIOFactory;
+
+/** A FileIOFactory that always returns the same FileIO instance. */
+public class TestFileIOFactory implements FileIOFactory {
+
+  private final FileIO fileIO;
+
+  public TestFileIOFactory(@Nonnull FileIO fileIO) {
+    this.fileIO = fileIO;
+  }
+
+  @Override
+  public FileIO loadFileIO(
+      @Nonnull CallContext callContext,
+      @Nonnull String ioImplClassName,
+      @Nonnull Map<String, String> properties,
+      @Nonnull TableIdentifier identifier,
+      @Nonnull Set<String> tableLocations,
+      @Nonnull Set<PolarisStorageActions> storageActions,
+      @Nonnull PolarisResolvedPathWrapper resolvedEntityPath) {
+    return fileIO;
+  }
+}
