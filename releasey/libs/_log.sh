8c1d4737f8ce1fc213f66c81e0d64e91205c5a7c
Pierre Laporte <pierre@pingtimeout.fr>
2025-07-23 10:20:26 +0200

    Rename NC to RESET

--- releasey/libs/_log.sh
+++ releasey/libs/_log.sh
@@ -27,18 +27,18 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+RESET='\033[0m' # No Color
 
 function print_error() {
-  echo -e "${RED}ERROR: $*${NC}" >&2
+  echo -e "${RED}ERROR: $*${RESET}" >&2
 }
 
 function print_success() {
-  echo -e "${GREEN}SUCCESS: $*${NC}" >&2
+  echo -e "${GREEN}SUCCESS: $*${RESET}" >&2
 }
 
 function print_warning() {
-  echo -e "${YELLOW}WARNING: $*${NC}" >&2
+  echo -e "${YELLOW}WARNING: $*${RESET}" >&2
 }
 
 function print_info() {
@@ -63,6 +63,6 @@ function print_command() {
   if { >&3; } 2>/dev/null; then
     echo "$*" >&3
   else
-    echo -e "${BLUE}DEBUG: $*${NC}" >&2
+    echo -e "${BLUE}DEBUG: $*${RESET}" >&2
   fi
 }
