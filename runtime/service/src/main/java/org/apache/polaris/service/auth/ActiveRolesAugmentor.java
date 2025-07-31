b42ce29e48f1942d21cb48cac6514447417b95fc
Alexandre Dutra <adutra@apache.org>
2025-07-31 12:13:26 +0200

    Rename packages, remove ".quarkus"

---
+++ runtime/service/src/main/java/org/apache/polaris/service/auth/ActiveRolesAugmentor.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.polaris.service.auth;
+
+import io.quarkus.security.AuthenticationFailedException;
+import io.quarkus.security.identity.AuthenticationRequestContext;
+import io.quarkus.security.identity.SecurityIdentity;
+import io.quarkus.security.identity.SecurityIdentityAugmentor;
+import io.quarkus.security.runtime.QuarkusSecurityIdentity;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import java.util.Set;
+import org.apache.polaris.core.auth.AuthenticatedPolarisPrincipal;
+import org.apache.polaris.service.auth.ActiveRolesProvider;
+
+/**
+ * A custom {@link SecurityIdentityAugmentor} that adds active roles to the {@link
+ * SecurityIdentity}. This is used to augment the identity with valid active roles after
+ * authentication.
+ */
+@ApplicationScoped
+public class ActiveRolesAugmentor implements SecurityIdentityAugmentor {
+
+  // must run after AuthenticatingAugmentor
+  public static final int PRIORITY = AuthenticatingAugmentor.PRIORITY - 1;
+
+  private final ActiveRolesProvider activeRolesProvider;
+
+  @Inject
+  public ActiveRolesAugmentor(ActiveRolesProvider activeRolesProvider) {
+    this.activeRolesProvider = activeRolesProvider;
+  }
+
+  @Override
+  public int priority() {
+    return PRIORITY;
+  }
+
+  @Override
+  public Uni<SecurityIdentity> augment(
+      SecurityIdentity identity, AuthenticationRequestContext context) {
+    if (identity.isAnonymous()) {
+      return Uni.createFrom().item(identity);
+    }
+    return context.runBlocking(() -> validateActiveRoles(identity));
+  }
+
+  private SecurityIdentity validateActiveRoles(SecurityIdentity identity) {
+    if (!(identity.getPrincipal() instanceof AuthenticatedPolarisPrincipal)) {
+      throw new AuthenticationFailedException("No Polaris principal found");
+    }
+    AuthenticatedPolarisPrincipal polarisPrincipal =
+        identity.getPrincipal(AuthenticatedPolarisPrincipal.class);
+    Set<String> validRoleNames = activeRolesProvider.getActiveRoles(polarisPrincipal);
+    return QuarkusSecurityIdentity.builder()
+        .setAnonymous(false)
+        .setPrincipal(polarisPrincipal)
+        .addRoles(validRoleNames) // replace the current roles with valid ones
+        .addCredentials(identity.getCredentials())
+        .addAttributes(identity.getAttributes())
+        .addPermissionChecker(identity::checkPermission)
+        .build();
+  }
+}
---
+++ runtime/service/src/main/java/org/apache/polaris/service/logging/QuarkusLoggingMDCFilter.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.polaris.service.logging;
+
+import static org.apache.polaris.service.context.RealmContextFilter.REALM_CONTEXT_KEY;
+
+import jakarta.annotation.Priority;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.PreMatching;
+import jakarta.ws.rs.ext.Provider;
+import org.apache.polaris.core.context.RealmContext;
+import org.apache.polaris.service.config.QuarkusFilterPriorities;
+import org.slf4j.MDC;
+
+@PreMatching
+@ApplicationScoped
+@Priority(QuarkusFilterPriorities.MDC_FILTER)
+@Provider
+public class QuarkusLoggingMDCFilter implements ContainerRequestFilter {
+
+  public static final String REALM_ID_KEY = "realmId";
+  public static final String REQUEST_ID_KEY = "requestId";
+
+  @Inject QuarkusLoggingConfiguration loggingConfiguration;
+
+  @Override
+  public void filter(ContainerRequestContext rc) {
+    // The request scope is active here, so any MDC values set here will be propagated to
+    // threads handling the request.
+    // Also put the MDC values in the request context for use by other filters and handlers
+    loggingConfiguration.mdc().forEach(MDC::put);
+    loggingConfiguration.mdc().forEach(rc::setProperty);
+    var requestId = rc.getHeaderString(loggingConfiguration.requestIdHeaderName());
+    if (requestId != null) {
+      MDC.put(REQUEST_ID_KEY, requestId);
+      rc.setProperty(REQUEST_ID_KEY, requestId);
+    }
+    RealmContext realmContext = (RealmContext) rc.getProperty(REALM_CONTEXT_KEY);
+    MDC.put(REALM_ID_KEY, realmContext.getRealmIdentifier());
+  }
+}
