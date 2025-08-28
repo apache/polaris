b33c3210acf4bd742717c452d0b2f0bf1bb05e7d
Adnan Hemani <adnan.h@berkeley.edu>
2025-08-28 13:06:27 -0700

    Add Delegator to all API Implementations (#2434)
    Per the Dev ML, implements the Delegator pattern to add Events instrumentation to all Polaris APIs.

---
+++ runtime/service/src/main/java/org/apache/polaris/service/catalog/generic/CatalogGenericTableEventServiceDelegator.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.polaris.service.catalog.generic;
+
+import jakarta.annotation.Priority;
+import jakarta.decorator.Decorator;
+import jakarta.decorator.Delegate;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
+import org.apache.polaris.core.context.RealmContext;
+import org.apache.polaris.service.catalog.api.PolarisCatalogGenericTableApiService;
+import org.apache.polaris.service.catalog.common.CatalogAdapter;
+import org.apache.polaris.service.types.CreateGenericTableRequest;
+
+@Decorator
+@Priority(1000)
+public class CatalogGenericTableEventServiceDelegator
+    implements PolarisCatalogGenericTableApiService, CatalogAdapter {
+
+  @Inject @Delegate GenericTableCatalogAdapter delegate;
+
+  @Override
+  public Response createGenericTable(
+      String prefix,
+      String namespace,
+      CreateGenericTableRequest createGenericTableRequest,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.createGenericTable(
+        prefix, namespace, createGenericTableRequest, realmContext, securityContext);
+  }
+
+  @Override
+  public Response dropGenericTable(
+      String prefix,
+      String namespace,
+      String genericTable,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.dropGenericTable(
+        prefix, namespace, genericTable, realmContext, securityContext);
+  }
+
+  @Override
+  public Response listGenericTables(
+      String prefix,
+      String namespace,
+      String pageToken,
+      Integer pageSize,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.listGenericTables(
+        prefix, namespace, pageToken, pageSize, realmContext, securityContext);
+  }
+
+  @Override
+  public Response loadGenericTable(
+      String prefix,
+      String namespace,
+      String genericTable,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.loadGenericTable(
+        prefix, namespace, genericTable, realmContext, securityContext);
+  }
+}
---
+++ runtime/service/src/main/java/org/apache/polaris/service/catalog/iceberg/IcebergRestCatalogEventServiceDelegator.java
@@ -0,0 +1,313 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.polaris.service.catalog.iceberg;
+
+import jakarta.annotation.Priority;
+import jakarta.decorator.Decorator;
+import jakarta.decorator.Delegate;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
+import org.apache.iceberg.rest.requests.CommitTransactionRequest;
+import org.apache.iceberg.rest.requests.CreateNamespaceRequest;
+import org.apache.iceberg.rest.requests.CreateTableRequest;
+import org.apache.iceberg.rest.requests.CreateViewRequest;
+import org.apache.iceberg.rest.requests.RegisterTableRequest;
+import org.apache.iceberg.rest.requests.RenameTableRequest;
+import org.apache.iceberg.rest.requests.ReportMetricsRequest;
+import org.apache.iceberg.rest.requests.UpdateNamespacePropertiesRequest;
+import org.apache.polaris.core.context.RealmContext;
+import org.apache.polaris.service.catalog.api.IcebergRestCatalogApiService;
+import org.apache.polaris.service.types.CommitTableRequest;
+import org.apache.polaris.service.types.CommitViewRequest;
+import org.apache.polaris.service.types.NotificationRequest;
+
+@Decorator
+@Priority(1000)
+public class IcebergRestCatalogEventServiceDelegator implements IcebergRestCatalogApiService {
+
+  @Inject @Delegate IcebergCatalogAdapter delegate;
+
+  @Override
+  public Response createNamespace(
+      String prefix,
+      CreateNamespaceRequest createNamespaceRequest,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.createNamespace(prefix, createNamespaceRequest, realmContext, securityContext);
+  }
+
+  @Override
+  public Response listNamespaces(
+      String prefix,
+      String pageToken,
+      Integer pageSize,
+      String parent,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.listNamespaces(
+        prefix, pageToken, pageSize, parent, realmContext, securityContext);
+  }
+
+  @Override
+  public Response loadNamespaceMetadata(
+      String prefix, String namespace, RealmContext realmContext, SecurityContext securityContext) {
+    return delegate.loadNamespaceMetadata(prefix, namespace, realmContext, securityContext);
+  }
+
+  @Override
+  public Response namespaceExists(
+      String prefix, String namespace, RealmContext realmContext, SecurityContext securityContext) {
+    return delegate.namespaceExists(prefix, namespace, realmContext, securityContext);
+  }
+
+  @Override
+  public Response dropNamespace(
+      String prefix, String namespace, RealmContext realmContext, SecurityContext securityContext) {
+    return delegate.dropNamespace(prefix, namespace, realmContext, securityContext);
+  }
+
+  @Override
+  public Response updateProperties(
+      String prefix,
+      String namespace,
+      UpdateNamespacePropertiesRequest updateNamespacePropertiesRequest,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.updateProperties(
+        prefix, namespace, updateNamespacePropertiesRequest, realmContext, securityContext);
+  }
+
+  @Override
+  public Response createTable(
+      String prefix,
+      String namespace,
+      CreateTableRequest createTableRequest,
+      String accessDelegationMode,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.createTable(
+        prefix, namespace, createTableRequest, accessDelegationMode, realmContext, securityContext);
+  }
+
+  @Override
+  public Response listTables(
+      String prefix,
+      String namespace,
+      String pageToken,
+      Integer pageSize,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.listTables(
+        prefix, namespace, pageToken, pageSize, realmContext, securityContext);
+  }
+
+  @Override
+  public Response loadTable(
+      String prefix,
+      String namespace,
+      String table,
+      String accessDelegationMode,
+      String ifNoneMatchString,
+      String snapshots,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.loadTable(
+        prefix,
+        namespace,
+        table,
+        accessDelegationMode,
+        ifNoneMatchString,
+        snapshots,
+        realmContext,
+        securityContext);
+  }
+
+  @Override
+  public Response tableExists(
+      String prefix,
+      String namespace,
+      String table,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.tableExists(prefix, namespace, table, realmContext, securityContext);
+  }
+
+  @Override
+  public Response dropTable(
+      String prefix,
+      String namespace,
+      String table,
+      Boolean purgeRequested,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.dropTable(
+        prefix, namespace, table, purgeRequested, realmContext, securityContext);
+  }
+
+  @Override
+  public Response registerTable(
+      String prefix,
+      String namespace,
+      RegisterTableRequest registerTableRequest,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.registerTable(
+        prefix, namespace, registerTableRequest, realmContext, securityContext);
+  }
+
+  @Override
+  public Response renameTable(
+      String prefix,
+      RenameTableRequest renameTableRequest,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.renameTable(prefix, renameTableRequest, realmContext, securityContext);
+  }
+
+  @Override
+  public Response updateTable(
+      String prefix,
+      String namespace,
+      String table,
+      CommitTableRequest commitTableRequest,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.updateTable(
+        prefix, namespace, table, commitTableRequest, realmContext, securityContext);
+  }
+
+  @Override
+  public Response createView(
+      String prefix,
+      String namespace,
+      CreateViewRequest createViewRequest,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.createView(prefix, namespace, createViewRequest, realmContext, securityContext);
+  }
+
+  @Override
+  public Response listViews(
+      String prefix,
+      String namespace,
+      String pageToken,
+      Integer pageSize,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.listViews(
+        prefix, namespace, pageToken, pageSize, realmContext, securityContext);
+  }
+
+  @Override
+  public Response loadCredentials(
+      String prefix,
+      String namespace,
+      String table,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.loadCredentials(prefix, namespace, table, realmContext, securityContext);
+  }
+
+  @Override
+  public Response loadView(
+      String prefix,
+      String namespace,
+      String view,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.loadView(prefix, namespace, view, realmContext, securityContext);
+  }
+
+  @Override
+  public Response viewExists(
+      String prefix,
+      String namespace,
+      String view,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.viewExists(prefix, namespace, view, realmContext, securityContext);
+  }
+
+  @Override
+  public Response dropView(
+      String prefix,
+      String namespace,
+      String view,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.dropView(prefix, namespace, view, realmContext, securityContext);
+  }
+
+  @Override
+  public Response renameView(
+      String prefix,
+      RenameTableRequest renameTableRequest,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.renameView(prefix, renameTableRequest, realmContext, securityContext);
+  }
+
+  @Override
+  public Response replaceView(
+      String prefix,
+      String namespace,
+      String view,
+      CommitViewRequest commitViewRequest,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.replaceView(
+        prefix, namespace, view, commitViewRequest, realmContext, securityContext);
+  }
+
+  @Override
+  public Response commitTransaction(
+      String prefix,
+      CommitTransactionRequest commitTransactionRequest,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.commitTransaction(
+        prefix, commitTransactionRequest, realmContext, securityContext);
+  }
+
+  @Override
+  public Response reportMetrics(
+      String prefix,
+      String namespace,
+      String table,
+      ReportMetricsRequest reportMetricsRequest,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.reportMetrics(
+        prefix, namespace, table, reportMetricsRequest, realmContext, securityContext);
+  }
+
+  @Override
+  public Response sendNotification(
+      String prefix,
+      String namespace,
+      String table,
+      NotificationRequest notificationRequest,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
+    return delegate.sendNotification(
+        prefix, namespace, table, notificationRequest, realmContext, securityContext);
+  }
+}
