55808c20d540016e97cda5e94cbda2b16bb2f6c0
Yufei Gu <yufei.apache.org>
2025-09-03 11:29:56 -0700

    fix style issue

--- polaris-core/src/test/java/org/apache/polaris/core/policy/PolicyTypeTest.java
+++ polaris-core/src/test/java/org/apache/polaris/core/policy/PolicyTypeTest.java
@@ -18,14 +18,14 @@
  */
 package org.apache.polaris.core.policy;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class PolicyTypeTest {
 
