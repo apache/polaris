d03c7174ccf6771a0673e02b5e6d691d6cca9419
Adnan Hemani <adnan.h@berkeley.edu>
2025-09-17 14:44:19 -0700

    Add Events for Iceberg REST APIs (#2480)

--- runtime/service/src/main/java/org/apache/polaris/service/admin/PolarisServiceImpl.java
+++ runtime/service/src/main/java/org/apache/polaris/service/admin/PolarisServiceImpl.java
@@ -76,7 +76,6 @@ import org.apache.polaris.service.admin.api.PolarisCatalogsApiService;
 import org.apache.polaris.service.admin.api.PolarisPrincipalRolesApiService;
 import org.apache.polaris.service.admin.api.PolarisPrincipalsApiService;
 import org.apache.polaris.service.config.ReservedProperties;
-import org.apache.polaris.service.events.AfterCatalogCreatedEvent;
 import org.apache.polaris.service.events.listeners.PolarisEventListener;
 import org.apache.polaris.service.types.PolicyIdentifier;
 import org.slf4j.Logger;
@@ -131,7 +130,6 @@ public class PolarisServiceImpl
     validateExternalCatalog(catalog);
     Catalog newCatalog = CatalogEntity.of(adminService.createCatalog(request)).asCatalog();
     LOGGER.info("Created new catalog {}", newCatalog);
-    polarisEventListener.onAfterCatalogCreated(new AfterCatalogCreatedEvent(newCatalog.getName()));
     return Response.status(Response.Status.CREATED).build();
   }
 
