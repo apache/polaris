c9097f713ece48798c1a6a79ba775475b795148b
cccs-cat001 <reginald.arsenault@cyber.gc.ca>
2025-10-27 08:32:12 -0300

    reworked iceberg metrics logging to be one class with configurable logging levels

--- runtime/service/src/main/java/org/apache/polaris/service/reporting/MetricsReportingConfiguration.java
+++ runtime/service/src/main/java/org/apache/polaris/service/reporting/MetricsReportingConfiguration.java
@@ -3,7 +3,7 @@ package org.apache.polaris.service.reporting;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithDefault;
 
-@ConfigMapping(prefix = "polaris.metrics.reporting")
+@ConfigMapping(prefix = "polaris.iceberg-metrics.reporting")
 public interface MetricsReportingConfiguration {
   @WithDefault("default")
   String type();
