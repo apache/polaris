6749010f49b5d3fa02c030feff526fb3286340e8
arun.suri <arun.suri@fivetran.com>
2025-10-30 07:51:25 +0530

    initial commit

--- runtime/service/build.gradle.kts
+++ runtime/service/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
   implementation(project(":polaris-api-management-service"))
   implementation(project(":polaris-api-iceberg-service"))
   implementation(project(":polaris-api-catalog-service"))
+  implementation(project(":polaris-version"))
 
   runtimeOnly(project(":polaris-relational-jdbc"))
 
