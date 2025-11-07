39ab58ceea83238083220c8864291a221dab5946
Robert Stupp <snazy@snazy.de>
2025-11-07 17:33:28 +0100

    NoSQL: Add (micro-ish) benchmarks
    A project for JMH based benchmarks against NoSQL persistence.

---
+++ persistence/nosql/persistence/benchmark/src/main/java/org/apache/polaris/persistence/nosql/benchmark/ImmutableObj.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.polaris.persistence.nosql.benchmark;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.apache.polaris.immutables.PolarisImmutable;
+import org.apache.polaris.persistence.nosql.api.obj.AbstractObjType;
+import org.apache.polaris.persistence.nosql.api.obj.Obj;
+import org.apache.polaris.persistence.nosql.api.obj.ObjType;
+import org.immutables.value.Value;
+
+@PolarisImmutable
+@JsonSerialize(as = ImmutableImmutableObj.class)
+@JsonDeserialize(as = ImmutableImmutableObj.class)
+public interface ImmutableObj extends Obj {
+  ObjType TYPE = new ImmutableObjType();
+
+  @Override
+  default ObjType type() {
+    return TYPE;
+  }
+
+  @Value.Default
+  default String value() {
+    return "foo";
+  }
+
+  @Override
+  @Value.Default
+  default String versionToken() {
+    return "vt";
+  }
+
+  final class ImmutableObjType extends AbstractObjType<ImmutableObj> {
+    public ImmutableObjType() {
+      super("jmh-imm", "immutable", ImmutableObj.class);
+    }
+  }
+}
