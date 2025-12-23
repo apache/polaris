7ad0de3527d4e77efc87a5d16b4a0c9496b64f24
Adam Christian <adam.christian@dremio.com>
2025-12-23 07:05:03 -0800

    Polaris Core SPI Proposal

--- polaris-core-spi/build.gradle.kts
+++ polaris-core-spi/build.gradle.kts
@@ -17,11 +17,54 @@
  * under the License.
  */
 
-plugins {
-  id("polaris-client")
-}
+plugins { id("polaris-client") }
 
 dependencies {
   compileOnly(libs.jakarta.annotation.api)
-}
+  compileOnly(libs.guava)
 
+  compileOnly(platform(libs.jackson.bom))
+  compileOnly("com.fasterxml.jackson.core:jackson-annotations")
+  compileOnly("com.fasterxml.jackson.core:jackson-databind")
+  compileOnly("com.fasterxml.jackson.dataformat:jackson-dataformat-smile")
+  compileOnly("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
+
+  compileOnly(project(":polaris-immutables"))
+  annotationProcessor(project(":polaris-immutables", configuration = "processor"))
+
+  compileOnly(project(":polaris-api-management-model"))
+
+  compileOnly(platform(libs.iceberg.bom))
+  compileOnly("org.apache.iceberg:iceberg-api")
+  compileOnly("org.apache.iceberg:iceberg-core")
+  compileOnly("org.apache.iceberg:iceberg-aws")
+  compileOnly("org.apache.iceberg:iceberg-azure")
+  compileOnly("org.apache.iceberg:iceberg-gcp")
+
+  compileOnly(platform(libs.awssdk.bom))
+  compileOnly("software.amazon.awssdk:sts")
+  compileOnly("software.amazon.awssdk:iam-policy-builder")
+  compileOnly("software.amazon.awssdk:s3")
+  compileOnly("software.amazon.awssdk:kms")
+
+  compileOnly(platform(libs.azuresdk.bom))
+  compileOnly("com.azure:azure-storage-blob")
+  compileOnly("com.azure:azure-storage-common")
+  compileOnly("com.azure:azure-identity")
+  compileOnly("com.azure:azure-storage-file-datalake")
+
+  compileOnly(platform(libs.google.cloud.storage.bom))
+  compileOnly("com.google.cloud:google-cloud-storage")
+  compileOnly(libs.google.cloud.iamcredentials)
+
+  compileOnly(libs.caffeine)
+  compileOnly(libs.slf4j.api)
+
+  testCompileOnly(project(":polaris-immutables"))
+  testAnnotationProcessor(project(":polaris-immutables", configuration = "processor"))
+
+  testImplementation(platform(libs.jackson.bom))
+  testImplementation("com.fasterxml.jackson.core:jackson-databind")
+  testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
+  testImplementation(libs.guava)
+}
