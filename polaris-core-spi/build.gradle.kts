067f250c9582512d54e0ad346c0d07fffafeb8ed
Adam Christian <adam.christian@dremio.com>
2025-12-23 07:04:39 -0800

    Polaris Core SPI Proposal

--- polaris-core-spi/build.gradle.kts
+++ polaris-core-spi/build.gradle.kts
@@ -16,13 +16,12 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.polaris.core.context;
 
-/**
- * Represents the elements of a REST request associated with routing to independent and isolated
- * "universes". This may include properties such as region, deployment environment (e.g. dev, qa,
- * prod), and/or account.
- */
-public interface RealmContext {
-  String getRealmIdentifier();
+plugins {
+  id("polaris-client")
 }
+
+dependencies {
+  compileOnly(libs.jakarta.annotation.api)
+}
+
