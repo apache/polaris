b8295baf996596e3918a60f7b73caaa6997f0544
Dmitri Bourlatchkov <dmitri.bourlatchkov@gmail.com>
2025-12-29 11:30:25 -0500

    review: update _has_aws_storage_info

--- client/python/apache_polaris/cli/command/catalogs.py
+++ client/python/apache_polaris/cli/command/catalogs.py
@@ -183,7 +183,17 @@ class CatalogsCommand(Command):
                 )
 
     def _has_aws_storage_info(self):
-        return self.role_arn or self.external_id or self.user_arn or self.region or self.endpoint or self.endpoint_internal or self.sts_endpoint or self.path_style_access
+        return (self.role_arn
+                or self.external_id
+                or self.user_arn
+                or self.region
+                or self.endpoint
+                or self.endpoint_internal
+                or self.sts_endpoint
+                or self.current_kms_key
+                or self.allowed_kms_keys
+                or self.path_style_access
+                )
 
     def _has_azure_storage_info(self):
         return self.tenant_id or self.multi_tenant_app_name or self.consent_url
