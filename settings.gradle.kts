3a7ccacdd49a853b5553dfc2aaff73f5e5d4f106
Robert Stupp <snazy@snazy.de>
2026-01-22 10:09:27 +0100

    Add jitpack.io repository
    The Iceberg PR https://github.com/apache/iceberg/pull/14991 bumps the https://github.com/RoaringBitmap/RoaringBitmap version, which is not published to Maven Central. We need to declare in the jitpack.io repository to prepare for the next Iceberg release.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -118,6 +118,7 @@ dependencyResolutionManagement {
   repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
   repositories {
     mavenCentral()
+    maven { url = uri("https://jitpack.io") }
     gradlePluginPortal()
   }
 }
