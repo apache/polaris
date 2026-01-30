74c4c9b222bb782001cd859f58897d7b3cfd4749
Robert Stupp <snazy@snazy.de>
2026-01-30 13:08:09 +0100

    Automatically adjust gradlew during Gradle Wrapper updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -248,3 +248,17 @@ tasks.register("showVersion") {
     )
   }
 }
+
+tasks.named<Wrapper>("wrapper") {
+  actions.addLast {
+    val script = scriptFile.readText()
+    val scriptLines = script.lines().toMutableList()
+
+    val insertAtLine =
+      scriptLines.indexOf("# Use the maximum available, or set MAX_FD != -1 to use that value.")
+    scriptLines.add(insertAtLine, "")
+    scriptLines.add(insertAtLine, $$". \"${APP_HOME}/gradle/gradlew-include.sh\"")
+
+    scriptFile.writeText(scriptLines.joinToString("\n"))
+  }
+}
