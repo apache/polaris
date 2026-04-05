1450649aa4ef1f2f419ae294f409a1875f7a499e
Yong Zheng <yongzheng0809@gmail.com>
2026-04-05 11:00:47 -0500

    CLI: add tables/find commands (#4075)

--- client/python/apache_polaris/cli/command/__init__.py
+++ client/python/apache_polaris/cli/command/__init__.py
@@ -219,6 +219,26 @@ class Command(ABC):
                 setup_config=options_get(Arguments.SETUP_CONFIG),
                 dry_run=options_get(Arguments.DRY_RUN),
             )
+        elif options.command == Commands.TABLES:
+            from apache_polaris.cli.command.tables import TableCommand
+
+            subcommand = options_get(f"{Commands.TABLES}_subcommand")
+            command = TableCommand(
+                subcommand,
+                catalog_name=options_get(Arguments.CATALOG),
+                namespace=options_get(
+                    Arguments.NAMESPACE, lambda x: x.split(".") if x else None
+                ),
+                table_name=options_get(Arguments.TABLE),
+            )
+        elif options.command == Commands.FIND:
+            from apache_polaris.cli.command.find import FindCommand
+
+            command = FindCommand(
+                options_get(Arguments.IDENTIFIER),
+                catalog_name=options_get(Arguments.CATALOG),
+                type_filter=options_get(Arguments.TYPE),
+            )
 
         if command is not None:
             command.validate()
