d1f2f8a3085a9fd1317ff8d3ca6b60a5b81da064
Hema Sri Shalini <hemasrishalini@gmail.com>
2026-06-03 22:49:11 +0530

    Add PolicyMappingUtil target entity type tests

--- polaris-core/src/test/java/org/apache/polaris/core/policy/PolicyMappingUtilTest.java
+++ polaris-core/src/test/java/org/apache/polaris/core/policy/PolicyMappingUtilTest.java
@@ -58,7 +58,7 @@ public class PolicyMappingUtilTest {
             PolicyMappingUtil.isValidTargetEntityType(
                 PolarisEntityType.TABLE_LIKE, null))
         .isFalse();
-
+    
     assertThat(
             PolicyMappingUtil.isValidTargetEntityType(
                 PolarisEntityType.TABLE_LIKE,
