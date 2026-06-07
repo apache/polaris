fa02494da23c6bdda1eae2ef319c3bfad5911061
Hema Sri Shalini <hemasrishalini@gmail.com>
2026-06-07 19:24:21 +0530

    Address review comments for PolicyMappingUtilTest

--- polaris-core/src/test/java/org/apache/polaris/core/policy/PolicyMappingUtilTest.java
+++ polaris-core/src/test/java/org/apache/polaris/core/policy/PolicyMappingUtilTest.java
@@ -20,49 +20,41 @@ package org.apache.polaris.core.policy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
 import org.apache.polaris.core.entity.PolarisEntitySubType;
 import org.apache.polaris.core.entity.PolarisEntityType;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class PolicyMappingUtilTest {
 
-  @Test
-  public void testValidTargetEntityTypes() {
-    assertThat(
-            PolicyMappingUtil.isValidTargetEntityType(
-                PolarisEntityType.CATALOG, null))
-        .isTrue();
-
-    assertThat(
-            PolicyMappingUtil.isValidTargetEntityType(
-                PolarisEntityType.NAMESPACE, null))
-        .isTrue();
-
-    assertThat(
-            PolicyMappingUtil.isValidTargetEntityType(
-                PolarisEntityType.TABLE_LIKE,
-                PolarisEntitySubType.ICEBERG_TABLE))
-        .isTrue();
+  static Stream<Arguments> validTargetEntityTypes() {
+    return Stream.of(
+        Arguments.of(PolarisEntityType.CATALOG, null),
+        Arguments.of(PolarisEntityType.NAMESPACE, null),
+        Arguments.of(PolarisEntityType.TABLE_LIKE, PolarisEntitySubType.ICEBERG_TABLE));
   }
 
-  @Test
-  public void testInvalidTargetEntityTypes() {
-    assertThat(PolicyMappingUtil.isValidTargetEntityType(null, null)).isFalse();
+  static Stream<Arguments> invalidTargetEntityTypes() {
+    return Stream.of(
+        Arguments.of(null, null),
+        Arguments.of(PolarisEntityType.PRINCIPAL_ROLE, null),
+        Arguments.of(PolarisEntityType.TABLE_LIKE, null),
+        Arguments.of(PolarisEntityType.TABLE_LIKE, PolarisEntitySubType.ICEBERG_VIEW));
+  }
 
-    assertThat(
-            PolicyMappingUtil.isValidTargetEntityType(
-                PolarisEntityType.PRINCIPAL_ROLE, null))
-        .isFalse();
+  @ParameterizedTest
+  @MethodSource("validTargetEntityTypes")
+  public void testValidTargetEntityTypes(
+      PolarisEntityType entityType, PolarisEntitySubType entitySubType) {
+    assertThat(PolicyMappingUtil.isValidTargetEntityType(entityType, entitySubType)).isTrue();
+  }
 
-    assertThat(
-            PolicyMappingUtil.isValidTargetEntityType(
-                PolarisEntityType.TABLE_LIKE, null))
-        .isFalse();
-    
-    assertThat(
-            PolicyMappingUtil.isValidTargetEntityType(
-                PolarisEntityType.TABLE_LIKE,
-                PolarisEntitySubType.ICEBERG_VIEW))
-        .isFalse();
+  @ParameterizedTest
+  @MethodSource("invalidTargetEntityTypes")
+  public void testInvalidTargetEntityTypes(
+      PolarisEntityType entityType, PolarisEntitySubType entitySubType) {
+    assertThat(PolicyMappingUtil.isValidTargetEntityType(entityType, entitySubType)).isFalse();
   }
 }
