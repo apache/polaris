56209565003fc231432c32e5495d2a08b9320908
Robert Stupp <snazy@snazy.de>
2026-06-12 17:59:19 +0200

    polaris-admin/test: Centralize Quarkus test profiles

---
+++ runtime/admin/src/testFixtures/java/org/apache/polaris/admintool/AdminProfiles.java
@@ -0,0 +1,138 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.polaris.admintool;
+
+import com.google.common.collect.ImmutableMap;
+import io.quarkus.test.junit.QuarkusTestProfile;
+import java.util.List;
+import java.util.Map;
+import org.apache.polaris.test.commons.CockroachRelationalJdbcLifeCycleManagement;
+import org.apache.polaris.test.commons.PostgresRelationalJdbcLifeCycleManagement;
+
+public final class AdminProfiles {
+
+  private static final Map<String, String> PRE_BOOTSTRAP_CONFIG = Map.of("pre-bootstrap", "true");
+
+  private AdminProfiles() {}
+
+  public static class NoSqlInMemory implements QuarkusTestProfile {
+    @Override
+    public Map<String, String> getConfigOverrides() {
+      return Map.of(
+          "polaris.persistence.type", "nosql", "polaris.persistence.nosql.backend", "InMemory");
+    }
+  }
+
+  public static class PreBootstrappedNoSqlInMemory extends NoSqlInMemory {
+    @Override
+    public Map<String, String> getConfigOverrides() {
+      return ImmutableMap.<String, String>builder()
+          .putAll(super.getConfigOverrides())
+          .putAll(PRE_BOOTSTRAP_CONFIG)
+          .build();
+    }
+  }
+
+  public static class NoSqlMongo implements QuarkusTestProfile {
+    @Override
+    public Map<String, String> getConfigOverrides() {
+      return Map.of(
+          "polaris.persistence.type", "nosql", "polaris.persistence.nosql.backend", "MongoDb");
+    }
+
+    @Override
+    public List<TestResourceEntry> testResources() {
+      return List.of(new TestResourceEntry(MongoTestResourceLifecycleManager.class));
+    }
+  }
+
+  public static class PreBootstrappedNoSqlMongo extends NoSqlMongo {
+    @Override
+    public Map<String, String> getConfigOverrides() {
+      return ImmutableMap.<String, String>builder()
+          .putAll(super.getConfigOverrides())
+          .putAll(PRE_BOOTSTRAP_CONFIG)
+          .build();
+    }
+  }
+
+  public static class RelationalJdbc implements QuarkusTestProfile {
+    @Override
+    public Map<String, String> getConfigOverrides() {
+      return Map.of(
+          "polaris.persistence.type",
+          "relational-jdbc",
+          // These two options are required to "trigger" the enablement of JDBC data sources in
+          // admin-tool tests, but do not harm other tests.
+          "quarkus.datasource.active",
+          "true",
+          "quarkus.datasource.db-kind",
+          "postgresql");
+    }
+
+    @Override
+    public List<TestResourceEntry> testResources() {
+      return List.of(
+          new TestResourceEntry(PostgresRelationalJdbcLifeCycleManagement.class, Map.of()));
+    }
+  }
+
+  public static class PreBootstrappedRelationalJdbc extends RelationalJdbc {
+    @Override
+    public Map<String, String> getConfigOverrides() {
+      return ImmutableMap.<String, String>builder()
+          .putAll(super.getConfigOverrides())
+          .putAll(PRE_BOOTSTRAP_CONFIG)
+          .build();
+    }
+  }
+
+  public static class CockroachJdbc implements QuarkusTestProfile {
+    @Override
+    public Map<String, String> getConfigOverrides() {
+      return Map.of(
+          "polaris.persistence.type",
+          "relational-jdbc",
+          "polaris.persistence.relational.jdbc.database-type",
+          "cockroachdb",
+          // These two options are required to "trigger" the enablement of JDBC data sources in
+          // admin-tool tests, but do not harm other tests.
+          "quarkus.datasource.active",
+          "true",
+          "quarkus.datasource.db-kind",
+          "postgresql");
+    }
+
+    @Override
+    public List<TestResourceEntry> testResources() {
+      return List.of(
+          new TestResourceEntry(CockroachRelationalJdbcLifeCycleManagement.class, Map.of()));
+    }
+  }
+
+  public static class PreBootstrappedCockroachJdbc extends CockroachJdbc {
+    @Override
+    public Map<String, String> getConfigOverrides() {
+      return ImmutableMap.<String, String>builder()
+          .putAll(super.getConfigOverrides())
+          .putAll(PRE_BOOTSTRAP_CONFIG)
+          .build();
+    }
+  }
+}
