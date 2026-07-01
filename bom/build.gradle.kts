dae17ca1140d294c4bc0c00aa0c1d7c785b3ef8b
Yufei Gu <yufei@apache.org>
2026-07-01 11:16:46 -0700

    Feat: Add OSI semantic-model API scaffolding (#4816)

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -99,6 +99,7 @@ dependencies {
     api(project(":polaris-extensions-federation-bigquery"))
     api(project(":polaris-extensions-federation-hadoop"))
     api(project(":polaris-extensions-federation-hive"))
+    api(project(":polaris-extensions-semantic-models"))
     api(project(":polaris-hms-testcontainer"))
 
     api(project(":polaris-spark-3.5_2.12"))
