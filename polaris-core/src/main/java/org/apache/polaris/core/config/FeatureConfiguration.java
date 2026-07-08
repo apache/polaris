458e7f1549448909c499534a28cddf3f69507e1c
iting0321 <iting0321@MacBook-11111156.local>
2026-07-08 17:10:04 +0900

    remove lineage realm feature gate

--- polaris-core/src/main/java/org/apache/polaris/core/config/FeatureConfiguration.java
+++ polaris-core/src/main/java/org/apache/polaris/core/config/FeatureConfiguration.java
@@ -537,13 +537,6 @@ public class FeatureConfiguration<T> extends PolarisConfiguration<T> {
           .defaultValue(false) // beta feature, keep it off by default
           .buildFeatureConfiguration();
 
-  public static final FeatureConfiguration<Boolean> ENABLE_LINEAGE =
-      PolarisConfiguration.<Boolean>builder()
-          .key("ENABLE_LINEAGE")
-          .description("If true, lineage services are enabled")
-          .defaultValue(false)
-          .buildFeatureConfiguration();
-
   public static final FeatureConfiguration<List<String>> SUPPORTED_CATALOG_CONNECTION_TYPES =
       PolarisConfiguration.<List<String>>builder()
           .key("SUPPORTED_CATALOG_CONNECTION_TYPES")
