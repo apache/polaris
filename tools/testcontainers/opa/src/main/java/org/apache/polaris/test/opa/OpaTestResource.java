1770ef99f30032c0d72e2aef4f5712637f2fec4b
Alexandre Dutra <adutra@apache.org>
2026-07-17 20:08:19 +0200

    Support for external principals
    This change introduces support for fully external principals, that is, principals that are not backed by an entity in Polaris metastore.
    
    The new "principal mode" is configurable on a per-realm basis, and by default, internal mode is used (no changes).
    
    When a realm is configured to use external principals instead:
    
    - The authentication type for the realm must NOT be `internal` (i.e., an external IDP must be used).
    - The authorizer type must NOT be `internal` (i.e., an external PDP must be used).
    - The authenticator bypasses the metastore lookup when authenticating the principal.
    - The authenticator sets the principal roles directly from the credential's claims, as-is (no prefix stripping, no "ALL" pseudo-role expansion).
    - The resolver creates synthetic principal and principal role entities if asked by the authorizer to resolve the principal and/or principal roles.
    
    Note: in theory, an external authorizer shouldn't attempt to resolve principal roles, but for now it can happen, until the Authorizer SPI is fully refactored to allow more fine-grained resolutions.
    
    See design doc:
    
    https://docs.google.com/document/d/1VSoN1-QsAJGaM40oWTxeLIoYTp-XlfQM9nG-jqfb_-E/edit
    
    Prior work:
    
    https://github.com/apache/polaris/pull/3250
    
    GitHub issue:
    
    https://github.com/apache/polaris/issues/441

---
+++ tools/testcontainers/opa/src/main/java/org/apache/polaris/test/opa/OpaTestResource.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.polaris.test.opa;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import java.util.Map;
+
+/**
+ * A {@link QuarkusTestResourceLifecycleManager} that starts an <a
+ * href="https://www.openpolicyagent.org/">Open Policy Agent</a> (OPA) container, uploads a Rego
+ * policy, and points Polaris at it by returning {@code polaris.authorization.type=opa} and {@code
+ * polaris.authorization.opa.policy-uri}.
+ *
+ * <p>The Rego policy can be supplied via the {@value #REGO_POLICY_ARG} init argument; otherwise a
+ * default policy that denies everything is used. The policy must declare {@code package
+ * polaris.authz}.
+ */
+public class OpaTestResource implements QuarkusTestResourceLifecycleManager {
+
+  /**
+   * Init argument holding the Rego policy to upload. Must declare {@code package polaris.authz}.
+   */
+  public static final String REGO_POLICY_ARG = "rego-policy";
+
+  private static final String POLICY_NAME = "polaris/authz";
+
+  private static final String DEFAULT_POLICY =
+      """
+      package polaris.authz
+
+      default allow := false
+      """;
+
+  private OpaContainer opa;
+  private String regoPolicy;
+
+  @Override
+  public void init(Map<String, String> initArgs) {
+    regoPolicy = initArgs.getOrDefault(REGO_POLICY_ARG, DEFAULT_POLICY);
+  }
+
+  @Override
+  public Map<String, String> start() {
+    opa = new OpaContainer();
+    opa.start();
+    opa.createRegoPolicy(POLICY_NAME, regoPolicy);
+    return Map.of(
+        "polaris.authorization.type",
+        "opa",
+        "polaris.authorization.opa.policy-uri",
+        opa.getExternalUrl() + "v1/data/" + POLICY_NAME);
+  }
+
+  @Override
+  public void stop() {
+    if (opa != null) {
+      try {
+        opa.stop();
+      } finally {
+        opa = null;
+      }
+    }
+  }
+}
