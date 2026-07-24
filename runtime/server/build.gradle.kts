b1749c521cb80a83ae513e7d438ede557379b0eb
Mark Mc Keown <zzcgumk@googlemail.com>
2026-07-24 23:33:33 +0100

    Extension for publishings events to Kafka (#4923)
    Added extension for publishing Polaris events to a
      Kafka topic.
    
      Application properties are:
        polaris.event-listener.types=kafka
        polaris.event-listener.kafka.topic
        polaris.event-listener.kafka.synchronous-mode
        polaris.event-listener.kafka.properties
    
      polaris.event-listener.kafka.properties is used to pass
      a set of properties through to the Kafka producer, for
      example bootstrap.server.
    
      The Kafka ProducerRecord key is the UUID from the PolarisEvent
      Metadata EventID and the value is a JSON key/value map encoded
      as a String. TableIdentifiers are encoded as arrays of Strings.
    
      JSON message is generated in helper class to support future
      reuse.
    
      Tests use Kafka test container.
    
      Fixes #4115
    
    Co-authored-by: Mark Mc Keown <mark.mckeown@cirata.com>

--- runtime/server/build.gradle.kts
+++ runtime/server/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
   runtimeOnly(project(":polaris-extensions-federation-hadoop"))
   runtimeOnly(project(":polaris-extensions-auth-opa"))
   runtimeOnly(project(":polaris-extensions-auth-ranger"))
+  runtimeOnly(project(":polaris-extensions-events-kafka"))
   runtimeOnly(project(":polaris-extensions-semantic-models"))
   runtimeOnly(project(":polaris-extensions-metrics-reports"))
   runtimeOnly(project(":polaris-extensions-metrics-reports-jdbc"))
