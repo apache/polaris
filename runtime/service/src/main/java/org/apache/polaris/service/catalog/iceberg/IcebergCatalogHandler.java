f2fe2d4223d27569dff2b0bb6c9dffe72815db17
Paras Mishra <138565099+ParasdotDev@users.noreply.github.com>
2026-08-11 23:53:05 +0530

    Remove used listTables(Namespace) overload from IcebergCatalogHandler (#5226)

--- runtime/service/src/main/java/org/apache/polaris/service/catalog/iceberg/IcebergCatalogHandler.java
+++ runtime/service/src/main/java/org/apache/polaris/service/catalog/iceberg/IcebergCatalogHandler.java
@@ -382,13 +382,6 @@ public abstract class IcebergCatalogHandler extends CatalogHandler implements Au
     }
   }
 
-  public ListTablesResponse listTables(Namespace namespace) {
-    PolarisAuthorizableOperation op = PolarisAuthorizableOperation.LIST_TABLES;
-    authorizeBasicNamespaceOperationOrThrow(op, namespace);
-
-    return catalogHandlerUtils().listTables(baseCatalog, namespace);
-  }
-
   /**
    * Create a table.
    *
