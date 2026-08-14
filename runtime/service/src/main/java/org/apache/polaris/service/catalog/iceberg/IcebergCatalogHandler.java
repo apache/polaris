1871d8c68eced4a73c5d5abc1499c449edd016df
Dmitri Bourlatchkov <dmitri.bourlatchkov@gmail.com>
2026-08-14 12:28:32 -0400

    Remove spurious javadoc from IcebergCatalogHandler (#5271)
    Following up on #5229.
    
    Remove javadoc remnants that used to describe the removed `createTableDirect()` method.

--- runtime/service/src/main/java/org/apache/polaris/service/catalog/iceberg/IcebergCatalogHandler.java
+++ runtime/service/src/main/java/org/apache/polaris/service/catalog/iceberg/IcebergCatalogHandler.java
@@ -382,13 +382,6 @@ public abstract class IcebergCatalogHandler extends CatalogHandler implements Au
     }
   }
 
-  /**
-   * Create a table.
-   *
-   * @param namespace the namespace to create the table in
-   * @param request the table creation request
-   * @return ETagged {@link LoadTableResponse} to uniquely identify the table metadata
-   */
   public void authorizeCreateTableDirect(
       Namespace namespace, CreateTableRequest request, boolean delegationRequested) {
     if (delegationRequested) {
