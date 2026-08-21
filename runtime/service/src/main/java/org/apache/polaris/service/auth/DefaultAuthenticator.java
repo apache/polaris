5d48e5175f6192baca96ecbfd78d18a3ada6252d
Alexandre Dutra <adutra@apache.org>
2026-08-21 16:25:37 +0200

    fix merge conflict artifact

--- runtime/service/src/main/java/org/apache/polaris/service/auth/DefaultAuthenticator.java
+++ runtime/service/src/main/java/org/apache/polaris/service/auth/DefaultAuthenticator.java
@@ -189,7 +189,7 @@ public class DefaultAuthenticator implements Authenticator {
   protected Map<String, Object> resolvePrincipalAttributes(
       SecurityIdentity identity,
       @Nullable PrincipalEntity principalEntity,
-      PrincipalRoleSelection roleSelection) {
+      boolean allRolesRequested) {
     // Do not merge the security identity's attributes into the principal attributes:
     // these must stay separate.
     ImmutableMap.Builder<String, Object> principalAttributes = ImmutableMap.builder();
@@ -199,8 +199,7 @@ public class DefaultAuthenticator implements Authenticator {
     if (principalEntity != null) {
       principalAttributes
           .put(PolarisPrincipal.PRINCIPAL_ENTITY_ATTRIBUTE_KEY, principalEntity)
-          .put(
-              PolarisPrincipal.PRINCIPAL_ROLE_ALL_ATTRIBUTE_KEY, roleSelection.allRolesRequested());
+          .put(PolarisPrincipal.PRINCIPAL_ROLE_ALL_ATTRIBUTE_KEY, allRolesRequested);
     } else {
       principalAttributes.put(PolarisPrincipal.EXTERNAL_PRINCIPAL_ATTRIBUTE_KEY, true);
     }
