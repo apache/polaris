e623f655b0c2424cf3a270f50efd9433ed2a9a63
Alexandre Dutra <adutra@apache.org>
2026-08-26 13:02:38 +0200

    review

--- runtime/service/src/main/java/org/apache/polaris/service/auth/external/mapping/PrincipalMapper.java
+++ runtime/service/src/main/java/org/apache/polaris/service/auth/external/mapping/PrincipalMapper.java
@@ -38,11 +38,6 @@ public interface PrincipalMapper {
   /**
    * Maps the {@link SecurityIdentity} to a Polaris principal ID.
    *
-   * <p>When using internal principals backed by a {@link
-   * org.apache.polaris.core.entity.PrincipalEntity} in the metastore, either the principal ID or
-   * the principal name must be provided. If external principals are used instead, the principal ID
-   * is irrelevant.
-   *
    * @param identity the {@link SecurityIdentity} of the user
    * @return the Polaris principal ID, or an empty optional if no mapping is available
    */
@@ -50,11 +45,6 @@ public interface PrincipalMapper {
 
   /**
    * Maps the {@link SecurityIdentity} to a Polaris principal name.
-   *
-   * <p>When using internal principals backed by a {@link
-   * org.apache.polaris.core.entity.PrincipalEntity} in the metastore, either the principal ID or
-   * the principal name must be provided. If external principals are used instead, the principal
-   * name must be provided.
    *
    * @param identity the {@link SecurityIdentity} of the user
    * @return the Polaris principal name, or an empty optional if no mapping is available
